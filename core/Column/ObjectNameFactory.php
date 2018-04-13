aa1939d9ea20ed722378f4f805c510b2929deff4
Jordy Manner <jordy.manner@milkcreation.fr>
2018-04-13 19:20:51 +0000

    branch 1.0 -- Correctifs diverses

--- core/Column/ObjectNameFactory.php
+++ core/Column/ObjectNameFactory.php
@@ -27,6 +27,12 @@ final class objectNameFactory
     protected $addColumn = [];
 
     /**
+     * Liste des colonnes à supprimer
+     * @var array
+     */
+    protected $removeColumn = [];
+
+    /**
      * CONSTRUCTEUR
      *
      * @return void
@@ -51,30 +57,44 @@ final class objectNameFactory
         // Ajout des colonnes personnalisé
         if ($this->addColumn) :
             foreach ($this->addColumn as $name => $controller) :
-                $this->_init($name, $controller);
+                $this->_add($name, $controller);
             endforeach;
         endif;
+
+        switch ($this->objectType) :
+            case 'post_type' :
+                $this->appAddFilter('manage_edit-' . $this->objectName . '_columns', '_remove', 50, 2);
+                break;
+
+            case 'taxonomy' :
+                $this->appAddFilter('manage_edit-' . $this->objectName  . '_columns', '_remove', 50, 3);
+                break;
+
+            case 'custom' :
+                $this->appAddFilter('manage_' . $this->objectName . '_columns', '_remove', 50, 3);
+                break;
+        endswitch;
     }
 
     /**
-     * Instanciation des controleurs d'affichage de colonne
+     * Instanciation des controleurs d'affichage de colonne.
      *
-     * @param string $name Identifiant de qualification du controleur d'affichage
+     * @param string $name Identifiant de qualification du controleur d'affichage.
      * @param array|ColumnFactory $controller {
-     *      Liste des attributs de configuration|Classe de rappel
+     *      Liste des attributs de configuration|Classe de rappel.
      *
-     *      @var string $id Identifiant de qualification
-     *      @var string $object_type Type d'objet
-     *      @var string $object_name Identifiant de qualification du type d'objet
-     *      @var string $column_name Identifiant de qualification de la colonne
-     *      @var string $title Intitulé de la colonne
-     *      @var int $position Position de la colonne dans la table
-     *      @var string|callable Affichage du rendu de la cellule
+     *      @var string $id Identifiant de qualification.
+     *      @var string $object_type Type d'objet.
+     *      @var string $object_name Identifiant de qualification du type d'objet.
+     *      @var string $column_name Identifiant de qualification de la colonne.
+     *      @var string $title Intitulé de la colonne.
+     *      @var int $position Position de la colonne dans la table.
+     *      @var string|callable Affichage du rendu de la cellule.
      * }
      *
      * @return null|ColumnFactory
      */
-    private function _init($name, $controller)
+    private function _add($name, $controller)
     {
         if (is_array($controller)) :
             $args = $controller;
@@ -96,7 +116,36 @@ final class objectNameFactory
     }
 
     /**
-     * Ajout d'un colonne
+     * Suppression des colonnes.
+     *
+     * @param string $name Identifiant de qualification du controleur d'affichage
+     * @param array|ColumnFactory $controller {
+     *      Liste des attributs de configuration|Classe de rappel
+     *
+     *      @var string $id Identifiant de qualification
+     *      @var string $object_type Type d'objet
+     *      @var string $object_name Identifiant de qualification du type d'objet
+     *      @var string $column_name Identifiant de qualification de la colonne
+     *      @var string $title Intitulé de la colonne
+     *      @var int $position Position de la colonne dans la table
+     *      @var string|callable Affichage du rendu de la cellule
+     * }
+     *
+     * @return void
+     */
+    final public function _remove($columns)
+    {
+        if($removes = array_intersect($this->removeColumn, array_keys($columns))) :
+            foreach($removes as $name) :
+                unset($columns[$name]);
+            endforeach;
+        endif;
+
+        return $columns;
+    }
+
+    /**
+     * Ajout d'une colonne.
      *
      * @param string $name Identifiant de qualification du controleur d'affichage
      * @param string|array|ColumnFactory $controller {
@@ -114,7 +163,7 @@ final class objectNameFactory
     {
         if (did_action('tify_custom_columns_register_too_late')) :
             wp_die(
-                __('La déclaration des colonnes doit se faire avant l\'exécution de l\'action "tify_custom_columns_register"'),
+                __('La déclaration des colonnes à ajouter doit se faire avant l\'exécution de l\'action "tify_custom_columns_register"'),
                 'Déclaration en échec',
                 500
             );
@@ -122,6 +171,29 @@ final class objectNameFactory
         endif;
 
         $this->addColumn[$name] = $controller;
+
+        return $this;
+    }
+
+    /**
+     * Suppression d'un colonne.
+     *
+     * @param string $name Identifiant de qualification de la colonne à supprimer.
+     *
+     * @return $this
+     */
+    public function remove($name)
+    {
+        if (did_action('tify_custom_columns_register_too_late')) :
+            wp_die(
+                __('La déclaration des colonnes à supprimer doit se faire avant l\'exécution de l\'action "tify_custom_columns_register"'),
+                'Déclaration en échec',
+                500
+            );
+            exit;
+        endif;
+
+        array_push($this->removeColumn, $name);
 
         return $this;
     }
