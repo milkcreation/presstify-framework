e3d40e7636d9d4daa3958f93488cc5652776f017
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2018-04-26 12:14:40 +0000

    trunk -- premier déploiement 1.5

--- trunk/bin/app/Traits/App.php
+++ trunk/bin/app/Traits/App.php
@@ -22,6 +22,29 @@ use tiFy\Apps;
 trait App
 {
     /**
+     * GLOBALES D'ENVIRONNEMENT
+     */
+    /**
+     * Récupération du chemin absolue vers la racine de stockage de presstiFy.
+     *
+     * @return string
+     */
+    public function tFyAbsDir()
+    {
+        return tiFy::$AbsDir;
+    }
+
+    /**
+     * Récupération de l'url absolue vers la racine de stockage de presstiFy.
+     *
+     * @return string
+     */
+    public function tFyAbsUrl()
+    {
+        return tiFy::$AbsUrl;
+    }
+
+    /**
      * FORMATAGE
      */
     /**
---
+++ trunk/core/Helpers.php
@@ -0,0 +1,81 @@
+<?php
+
+use tiFy\tiFy;
+
+/**
+ * Partial
+ */
+/**
+ * Fil d'arianne
+ *
+ * @param array $attrs {
+ *      Liste des attributs de configuration
+ * }
+ * @param bool $echo Activation de l'affichage. défaut true.
+ *
+ * @return string
+ */
+function tify_partial_breadcrumb($attrs = [], $echo = true)
+{
+    $layout = (string)Layout::Breadcrumb($attrs);
+
+    if ($echo) :
+        echo $layout;
+    else :
+        return $layout;
+    endif;
+}
+
+/**
+ * Message de notification
+ *
+ * @param array $attrs {
+ *      Liste des attributs de configuration
+ *
+ *      @var string $id Identifiant de qualification du controleur d'affichage.
+ *      @var string $container_id ID HTML du conteneur de l'élé@var string $id Identifiant de qualification du controleur d'affichage.ment.
+ *      @var string $container_class Classes HTML du conteneur de l'élément.
+ *      @var string $text Texte de notification. défaut 'Lorem ipsum dolor site amet'.
+ *      @var string $dismissible Bouton de masquage de la notification.
+ *      @var string $type Type de notification info|warning|success|error. défaut info.
+ * }
+ * @param bool $echo Activation de l'affichage. défaut true.
+ *
+ * @return string
+ */
+function tify_partial_notice($attrs = [], $echo = true)
+{
+    $layout = (string)Layout::Notice($attrs);
+
+    if ($echo) :
+        echo $layout;
+    else :
+        return $layout;
+    endif;
+}
+
+/**
+ * Balise HTML
+ *
+ * @param array $attrs {
+ *      Liste des attributs de configuration
+ *
+ *      @var string $id Identifiant de qualification du controleur d'affichage.
+ *      @var string $tag Balise HTML div|span|a|... défaut div.
+ *      @var array $attrs Liste des attributs de balise HTML.
+ *      @var string $content Contenu de la balise HTML.
+ * }
+ * @param bool $echo Activation de l'affichage. défaut true.
+ *
+ * @return string
+ */
+function tify_partial_tag($attrs = [], $echo = true)
+{
+    $layout = (string)Layout::Tag($attrs);
+
+    if ($echo) :
+        echo $layout;
+    else :
+        return $layout;
+    endif;
+}
