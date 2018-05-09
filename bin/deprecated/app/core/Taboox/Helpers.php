9f28f2b498fc8a097ace0da040992d134afcc0a7
Jordy Manner <jordy.manner@milkcreation.fr>
2018-05-09 16:29:54 +0000

    tag 1.4.34 -- correctif bug field Label et Control Repeater

---
+++ bin/deprecated/app/core/Taboox/Helpers.php
@@ -0,0 +1,12 @@
+<?php
+namespace tiFy\Core\Taboox;
+
+abstract class Helpers extends \tiFy\App\Factory
+{
+    /* = ARGUMENTS = */
+    // Intitulés des prefixes des fonctions
+    protected $Prefix = 'tify_taboox';
+
+    // Liste des methodes à translater en Helpers
+    protected $Helpers = [];
+}
