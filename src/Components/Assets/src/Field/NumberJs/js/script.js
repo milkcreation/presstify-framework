26919c1ad3e3ec87e42bf4622b86eae39df77ecd
Jordy Manner <jordy.manner@milkcreation.fr>
2018-05-22 07:11:15 +0000

    tag 1.4.36 -- correctif bug lib Importer (break x)

--- src/Components/Assets/src/Field/NumberJs/js/script.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function ($) {
-    $('.tiFyField-NumberJs').each(function() {
-        var options = JSON.parse(
-            decodeURIComponent($(this).data('options'))
-        );
-        $(this).spinner(options);
-    });
-});
