6a274cfe1f68505df416151ea21ad290f70b0f54
julien.picard <julien.picard@59481347-5d8a-4110-b049-6281aa622e7a>
2018-05-24 13:04:37 +0000

    branch 1.4 -- correctif SelectJs + appel spinkit et dashicons webpack

--- branches/1.4/assets/SelectJs/index.js
+++ branches/1.4/assets/SelectJs/index.js
@@ -1,7 +1,7 @@
 
 /** Dépendances */
 // SpinKit
-import 'spinkit/scss/spinners/7-three-bounce.scss';
+import '../../bin/assets/vendor/spinkit/7-three-bounce.min.css';
 // jQueryUi
 import 'jquery-ui/ui/widget.js';
 import 'jquery-ui/ui/widgets/sortable'
