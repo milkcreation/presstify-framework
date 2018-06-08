ae3acf4cc4e0527bfc6f2276452c96f13a39959c
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2018-06-08 16:52:21 +0000

    branch2.0 -- migration v2 phase #11

---
+++ branches/2.0/src/Components/Field/SelectJs/templates/select-js.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @var \tiFy\Field\TemplateController $this
+ */
+?>
+
+<?php $this->before(); ?>
+
+<div <?php $this->attrs(); ?>>
+
+    <?php $this->partial('handler', $this->all()); ?>
+
+    <?php $this->partial('selected-items', $this->all()); ?>
+
+    <?php $this->partial('picker-items', $this->all()); ?>
+
+</div>
+
+
+<?php $this->after(); ?>
