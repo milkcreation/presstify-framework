0a4dcdc649d782e5e3352f9c42ad7be17c10aefc
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2018-06-11 09:44:17 +0000

    presstify-framework -- branch1.5 -- suppression version

--- branches/1.5/bin/App.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace tiFy;
-
-use tiFy\App\Traits\App as TraitsApp;
-
-abstract class App
-{
-    use TraitsApp;
-}
--- branches/1.5/core/Query/Controller/UserList.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace tiFy\Core\Query\Controller;
-
-final class UserList extends AbstractUserList
-{
-
-}
