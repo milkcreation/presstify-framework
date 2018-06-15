b2238fee22b84d0940f998773725ef6400fca1ef
Jordy Manner <jordy.manner@milkcreation.fr>
2018-06-15 17:18:29 +0000

    trunk -- migration v2 phase #13

--- src/Components/Partial/Spinner/templates/circle.php
+++ src/Components/Partial/Spinner/templates/circle.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * @var string $container_id Id HTML du conteneur
- * @var string $container_class Classe HTML du conteneur
- * @var string $type Type de controleur spinkit
+ * @var \tiFy\Partial\TemplateController $this
  */
 ?>
-<div id="<?php echo $container_id; ?>" class="tiFyCoreControl-spinkit tiFyCoreControl-spinkit--<?php echo $type; ?><?php echo $container_class ? " {$container_class}" : '';?> sk-circle">
+
+<?php $this->before(); ?>
+
+<div <?php $this->attrs(); ?>>
     <div class="sk-circle1 sk-child"></div>
     <div class="sk-circle2 sk-child"></div>
     <div class="sk-circle3 sk-child"></div>
@@ -19,3 +20,5 @@
     <div class="sk-circle11 sk-child"></div>
     <div class="sk-circle12 sk-child"></div>
 </div>
+
+<?php $this->after(); ?>
