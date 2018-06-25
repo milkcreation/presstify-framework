be2ebf950367a213601013715e7217ee7f8c644f
Jordy Manner <jordy.manner@milkcreation.fr>
2018-06-25 08:20:22 +0000

    branch2.0 -- Migrationv2 phase#17

--- src/Components/AdminView/PostListTable/PostListTableServiceProvider.php
+++ src/Components/AdminView/PostListTable/PostListTableServiceProvider.php
@@ -3,6 +3,7 @@
 namespace tiFy\Components\AdminView\PostListTable;
 
 use tiFy\Components\AdminView\ListTable\ListTableServiceProvider;
+use tiFy\Components\AdminView\PostListTable\Column\ColumnItemPostTitleController;
 use tiFy\Components\AdminView\PostListTable\ViewFilter\ViewFilterItemAllController;
 use tiFy\Components\AdminView\PostListTable\ViewFilter\ViewFilterItemPublishController;
 use tiFy\Components\AdminView\PostListTable\ViewFilter\ViewFilterItemTrashController;
@@ -17,6 +18,12 @@ class PostListTableServiceProvider extends ListTableServiceProvider
         return array_merge(
             parent::defaults(),
             [
+                'columns.item.post_title' => [
+                    'alias'     => ColumnItemPostTitleController::class,
+                    'concrete'  => $this->app->getConcrete('columns.item.post_title', ColumnItemPostTitleController::class),
+                    'bootable'  => false,
+                    'singleton' => false
+                ],
                 'view_filters.item.all' => [
                     'alias'     => ViewFilterItemAllController::class,
                     'concrete'  => $this->app->getConcrete('view_filters.item.all', ViewFilterItemAllController::class),
