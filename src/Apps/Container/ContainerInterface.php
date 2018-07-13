8f78d885a5ea8ffac9d59c9dcde08a15f261efd3
Jordy Manner <jordy.manner@milkcreation.fr>
2018-07-13 13:55:44 +0000

    trunk -- Migration v2 phase #27

---
+++ src/Apps/Container/ContainerInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tiFy\Apps\Container;
+
+use tiFy\Apps\AppControllerInterface;
+
+interface Container extends AppControllerInterface
+{
+    /**
+     * Récupération d'un service fourni.
+     *
+     * @param string $alias Identifiant de qualification du controleur.
+     * @param array $args Liste des variables passés en argument.
+     *
+     * @return object
+     */
+    public function resolve($alias, $args = [])
+}
---
+++ src/Apps/Container/ServiceInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace tiFy\Apps\Container;
+
+interface ServiceInterface
+{
+    /**
+     * Récupération de l'alias.
+     *
+     * @return string
+     */
+    public function getAlias();
+
+    /**
+     * Récupération de la liste des variables passées en argument.
+     *
+     * @return array
+     */
+    public function getArgs();
+
+    /**
+     * Récupération du controleur de service.
+     *
+     * @return callable|object
+     */
+    public function getConcrete();
+
+    /**
+     * Vérifie si un controleur de service est instancié au démarrage.
+     *
+     * @return bool
+     */
+    public function isBootable();
+
+    /**
+     * Vérifie si un controleur de service est une fonction anonyme.
+     *
+     * @return bool
+     */
+    public function isClosure();
+
+    /**
+     * Vérifie si un controleur de service est instancié de manière différé.
+     *
+     * @return bool
+     */
+    public function isDeferred();
+
+    /**
+     * Vérification d'existance d'une instanciation du controleur.
+     *
+     * @return bool
+     */
+    public function isInstanciated();
+
+    /**
+     * Vérifie si le controleur de service doit être traité comme un singleton.
+     * @internal Une seule instance unique de la classe dans l'écosystème
+     *
+     * @return bool
+     */
+    public function isSingleton();
+
+    /**
+     * Définition de la liste des variables passées en argument.
+     *
+     * @param array $args Liste des variables passées au controleur en argument
+     *
+     * @return $this
+     */
+    public function setArgs($args = []);
+
+    /**
+     * Définition de l'instanciation du controleur.
+     *
+     * @return bool
+     */
+    public function setInstanciated();
+}
