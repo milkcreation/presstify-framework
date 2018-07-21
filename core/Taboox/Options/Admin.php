21d1dd145605999ac6b2fec535c77391a7e8c584
Jordy Manner <jordy.manner@milkcreation.fr>
2018-07-21 10:10:24 +0000

    branch1.4 -- tag 1.4.52

---
+++ core/Taboox/Options/Admin.php
@@ -0,0 +1,17 @@
+<?php
+namespace tiFy\Core\Taboox\Options;
+
+use tiFy\Core\Taboox\Admin as TabooxAdmin;
+
+abstract class Admin extends TabooxAdmin
+{
+    /**
+     * Formulaire de saisie
+     *
+     * @return string
+     */
+    public function form()
+    {
+
+    }
+}
