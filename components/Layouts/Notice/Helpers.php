3fcef2c70897175a4573f00a71d5edf8a7ab6626
Jordy Manner <jordy.manner@milkcreation.fr>
2018-07-22 17:37:10 +0000

    tag1.4.54 -- Ajustement App mineurs

---
+++ components/Layouts/Notice/Helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+use tiFy\Core\Layout\Layout;
+
+/**
+ * Affichage d'un message de notification
+ *
+ * @param array $attrs {
+ *      Liste des attributs de configuration
+ *
+ *      @var string $container_id ID HTML du conteneur de l'élément.
+ *      @var string $container_class Classes HTML du conteneur de l'élément.
+ *      @var string $text Texte de notification. défaut 'Lorem ipsum dolor site amet'.
+ *      @var string $dismissible Bouton de masquage de la notification
+ *      @var string $type Type de notification info|warning|success|error. défaut info.
+ * }
+ * @return void
+ */
+function tify_layout_notice($attrs = [])
+{
+    echo (string)Layout::Notice($attrs);
+}
--- src/Components/Partial/Modal/templates/header.php
+++
@@ -1,6 +0,0 @@
-<div class="modal-header">
-    <h5 class="modal-title"><?php _e('Fenêtre de dialogue PresstiFy', 'tify'); ?></h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-</div>
