109dbaac7a7f5e8d7988a0aefddff29407a04679
louis.dherent <louis.dherent@59481347-5d8a-4110-b049-6281aa622e7a>
2018-11-14 10:49:24 +0000

    tags 1.4.75 -- Evolutions Repeater

--- tags/1.4.75/assets/field/repeater/js/scripts.js
+++ tags/1.4.75/assets/field/repeater/js/scripts.js
@@ -1,55 +1,55 @@
 jQuery(document).ready(function ($) {
     var jqxhr;
 
-    // Ajout d'un élément
-    $(document).on('click.tify.field.repeater.add', '[aria-control="repeater"] [aria-control="add"]', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        if (jqxhr !== undefined) {
-            return;
-        }
-
-        // Eléments du DOM
-        var $this = $(this),
-            $closest = $(this).closest('[aria-control="repeater"]'),
-            $items = $('[aria-control="items"]', $closest);
-
-        // Variables
-        var index = $('[aria-control="item"]', $items).length,
+    // Ajout d'un élément.
+    $('[aria-control="repeater"]').each(function() {
+        let $closest = $(this),
+            id = $closest.attr('aria-id'),
             o = JSON.parse(decodeURIComponent($closest.data('options')));
 
-        jqxhr = $.post(
-            tify_ajaxurl,
-            {
-                action: o.ajax_action,
-                _ajax_nonce: o.ajax_nonce,
-                index: index,
-                options: o
+        $(document).on('click.tify.field.repeater.add', '[aria-id="' + id + '"] [aria-control="add"]', function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+
+            if (jqxhr !== undefined) {
+                return;
             }
-        )
-            .done(function (resp) {
-                if (!resp.success) {
-                    alert(resp.data);
-                } else {
-                    $items.append(resp.data);
+
+            let $items = $('[aria-control="items"]', $closest),
+                index = $('[aria-control="item"]', $items).length;
+
+            jqxhr = $.post(
+                tify_ajaxurl,
+                {
+                    action: o.ajax_action,
+                    _ajax_nonce: o.ajax_nonce,
+                    index: index,
+                    options: o
                 }
-            })
-            .always(function () {
-                jqxhr = undefined;
+            )
+                .done(function (resp) {
+                    if (!resp.success) {
+                        alert(resp.data);
+                    } else {
+                        $items.append(resp.data);
+                    }
+                })
+                .always(function () {
+                    jqxhr = undefined;
+                });
+        });
+
+        // Ordonnacement des images de la galerie.
+        $('[aria-id="' + id + '"][aria-sortable="true"] [aria-control="items"]')
+            .sortable(o.sortable)
+            .disableSelection();
+
+        // Suppression d'un élément.
+        $(document).on('click.tify.field.repeater.remove', '[aria-id="' + id + '"] [aria-control="remove"]', function (e) {
+            e.preventDefault();
+            $(this).closest('[aria-control="item"]').fadeOut(function () {
+                $(this).remove();
             });
-    });
-
-    // Ordonnacement des images de la galerie
-    $('[aria-control="repeater"] [aria-control="items"]')
-        .sortable({placeholder: 'tiFyField-RepeaterItemPlaceholder', axis: 'y'})
-        .disableSelection();
-
-    // Suppression d'un élément
-    $(document).on('click.tify.field.repeater.remove', '[aria-control="repeater"] [aria-control="remove"]', function (e) {
-        e.preventDefault();
-        $(this).closest('[aria-control="item"]').fadeOut(function () {
-            $(this).remove();
         });
     });
 });
