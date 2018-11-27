9e89fb20bda16ce730ddeccf0354f8236ac5ebd1
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2018-11-27 08:13:03 +0000

    tag 2.0.17 -- Routing > ok + ajustement Contracts

---
+++ tags/2.0.17/src/Form/Factory/Fields.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace tiFy\Form\Factory;
+
+use Illuminate\Support\Collection;
+use tiFy\Contracts\Form\FactoryFields;
+use tiFy\Contracts\Form\FactoryField;
+use tiFy\Contracts\Form\FormFactory;
+use tiFy\Form\Factory\AbstractItemsIterator;
+use tiFy\Form\Factory\ResolverTrait;
+
+class Fields extends AbstractItemsIterator implements FactoryFields
+{
+    use ResolverTrait;
+
+    /**
+     * Liste des éléments associés au formulaire.
+     * @var FactoryField[]
+     */
+    protected $items = [];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param array $fields Liste des champs associés au formulaire.
+     * @param FormFactory $form Instance du contrôleur de formulaire.
+     *
+     * @return void
+     */
+    public function __construct($fields = [], FormFactory $form)
+    {
+        $this->form = $form;
+
+        // Déclaration des champs.
+        foreach ($fields as $slug => $attrs) :
+            if (!is_null($slug)) :
+                $this->items[$slug] = app()->singleton(
+                    "form.factory.field.{$this->form->name()}.{$slug}",
+                    function ($slug, $attrs = []) {
+                        return app()->resolve('form.factory.field', [$slug, $attrs, $this->form]);
+                    }
+                )->build([$slug, $attrs]);
+            endif;
+        endforeach;
+
+        // Ordonnacement des champs.
+        foreach ($this->byGroup() as $group) :
+            $max = $group->max(function (FactoryField $field) { return $field->getPosition(); });
+            $pad = 0;
+
+            $group->each(function (FactoryField $field, $key) use (&$pad, $max) {
+                $number = 10000 * ($field->getGroup()+1);
+                $position = $field->getPosition() ? : ++$pad+$max;
+
+                return $field->setPosition(absint($number+$position));
+            });
+        endforeach;
+
+        $this->items = $this->byPosition();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function byGroup()
+    {
+        return $this->collect()->groupBy(function (FactoryField $field) {
+            return $field->getGroup();
+        })->all();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function byPosition()
+    {
+        return $this->collect()->sortBy(function (FactoryField $field) {
+            return $field->getPosition();
+        })->all();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collect()
+    {
+        return new Collection($this->items);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasGroup()
+    {
+        return !empty(
+            $this->collect()->max(function (FactoryField $field) {
+                return $field->getGroup();
+            })
+        );
+    }
+}
---
+++ tags/2.0.17/src/User/Query/UserQueryCollection.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace tiFy\User\Query;
+
+use tiFy\Contracts\User\UserQueryCollection as UserQueryCollectionContract;
+use tiFy\Contracts\User\UserQueryItem;
+use tiFy\Kernel\Query\QueryCollection;
+
+class UserQueryCollection extends QueryCollection implements UserQueryCollectionContract
+{
+    /**
+     * Liste des éléments déclarés.
+     * @var UserQueryItem[] $items
+     */
+    protected $items = [];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIds()
+    {
+        return $this->collect()->pluck('ID')->all();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDisplayNames()
+    {
+        return $this->collect()->pluck('display_name')->all();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEmails()
+    {
+        return $this->collect()->pluck('user_email')->all();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLogins()
+    {
+        return $this->collect()->pluck('user_login')->all();
+    }
+}
