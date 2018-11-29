25a37a60ffdcea084db4318f04820b3fe7557b75
Jordy Manner <jordy.manner@milkcreation.fr>
2018-11-29 14:02:10 +0000

    tag 2.0.20 -- réécriture partielle de la metabox options slideshow

---
+++ src/Column/ColumnView.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace tiFy\Column;
+
+use tiFy\View\ViewController;
+
+/**
+ * Class ColumnView
+ */
+class ColumnView extends ViewController
+{
+    /**
+     * Liste des méthodes héritées.
+     * @var array
+     */
+    protected $mixins = [
+
+    ];
+
+    /**
+     * Translation d'appel des méthodes de l'application associée.
+     *
+     * @param string $name Nom de la méthode à appeler.
+     * @param array $arguments Liste des variables passées en argument.
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        if (in_array($name, $this->mixins)) :
+            return call_user_func_array(
+                [$this->engine->get('column'), $name],
+                $arguments
+            );
+        endif;
+    }
+}
---
+++ src/Contracts/Layout/LayoutMenuAdminInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace tiFy\Contracts\Layout;
+
+use tiFy\Contracts\Kernel\ParamsBag;
+
+interface LayoutMenuAdminInterface extends ParamsBag
+{
+
+}
