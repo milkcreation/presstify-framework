afca22ca2adb0098fda962f9b207e87aa85923eb
Jordy Manner <jordy.manner@milkcreation.fr>
2018-12-02 16:01:02 +0000

    tag 2.0.21 -- correctif Route + nettoyage code

---
+++ src/Layout/LayoutFrontFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace tiFy\Layout;
+
+use tiFy\Contracts\Layout\LayoutFrontFactoryInterface;
+use tiFy\Contracts\Layout\LayoutItemInterface;
+
+class LayoutFrontFactory extends AbstractLayoutFactory implements LayoutFrontFactoryInterface
+{
+    /**
+     * Liste des attributs de configuration.
+     * @var array {
+     *
+     *      @param string|callable $content Classe de rappel du controleur d'affichage.
+     *      @param array $params Liste des paramètres.
+     * }
+     */
+    protected $attributes = [];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param string $name Nom de qualification de la disposition associée.
+     * @param array $attrs Attributs de configuration de la disposition associée.
+     *
+     * @return void
+     */
+    public function __construct($name, $attrs = [])
+    {
+        parent::__construct($name, $attrs);
+
+        $this->add_action(
+            'wp_loaded',
+            function () {
+                if ($this->layout() instanceof LayoutItemInterface) :
+                    $this->layout()->load();
+                endif;
+            }
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isAdmin()
+    {
+        return false;
+    }
+}
