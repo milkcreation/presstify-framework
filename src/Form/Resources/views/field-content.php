2b054d2edbc4776f619c424cbfe43017158ae39c
Jordy Manner <jordy.manner@milkcreation.fr>
2018-12-04 13:53:17 +0000

    tag 2.0.22 -- Correctif Routing + Ajustement divers

---
+++ src/Form/Resources/views/field-content.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Zone de contenu du champ de formulaire.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Contracts\Form\FactoryView $this
+ * @var tiFy\Contracts\Form\FactoryField $field
+ */
+?>
+
+<?php echo $field; ?>
--- src/Kernel/Http/Request.php
+++ src/Kernel/Http/Request.php
@@ -3,11 +3,26 @@
 namespace tiFy\Kernel\Http;
 
 use Illuminate\Http\Request as IlluminateHttpRequest;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\HeaderBag;
+use Symfony\Component\HttpFoundation\FileBag;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\ServerBag;
+use Psr\Http\Message\ServerRequestInterface;
 
 class Request extends IlluminateHttpRequest
 {
     /**
-     * {@inheritdoc}
+     * Récupération de la classe de rappel de propriété de la requête globale.
+     * @see https://laravel.com/api/5.6/Illuminate/Http/Request.html
+     * @see https://symfony.com/doc/current/components/http_foundation.html
+     * @see http://api.symfony.com/4.0/Symfony/Component/HttpFoundation/ParameterBag.html
+     *
+     * @param string $property Propriété de la requête à traiter.
+     * $_POST (alias post, request)|$_GET (alias get, query)|$_COOKIE (alias cookie, cookies)|attributes
+     * |$_FILES (alias files)|SERVER (alias server)|headers.
+     *
+     * @return Request|FileBag|HeaderBag|ParameterBag|ServerBag
      */
     public function getProperty($property = '')
     {
@@ -40,5 +55,19 @@ class Request extends IlluminateHttpRequest
                 return $this->headers;
                 break;
         endswitch;
+    }
+
+    /**
+     * Création d'une instance depuis une requête PSR-7.
+     *
+     * @param ServerRequestInterface $psrRequest Requête PSR
+     *
+     * @return self|IlluminateHttpRequest
+     */
+    public function createFromPsr(ServerRequestInterface $psrRequest)
+    {
+        $request = (new HttpFoundationFactory())->createRequest($psrRequest);
+
+        return self::createFromBase($request);
     }
 }
