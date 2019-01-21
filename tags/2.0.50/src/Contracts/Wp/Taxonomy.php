fa412b57642a676c805253409f5ac20378395272
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-01-21 06:42:29 +0000

    tag 2.0.50 -- Phase réécriture injection deps Form + FormAddonRecord phase #1 + Phase nettoyage Db en vue de réécriture

---
+++ tags/2.0.50/src/Contracts/Wp/Taxonomy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace tiFy\Contracts\Wp;
+
+use WP_Term;
+
+interface Taxonomy
+{
+    /**
+     * Récupération de la liste des termes selon l'ordre.
+     * @see https://developer.wordpress.org/reference/classes/wp_term_query/
+     *
+     * @param string|array $taxonomy Nom ou liste de taxonomies associées.
+     * @param array $attrs Liste des arguments de récupération des éléments.
+     * @param string $order_meta_key Clé d'indice de la metadonnée de traitement de l'ordre
+     *
+     * @return WP_Term[]|int
+     */
+    public function getTermsByOrder($taxonomy, $args = [], $order_meta_key = '_order');
+}
