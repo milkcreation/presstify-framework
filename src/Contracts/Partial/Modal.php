65606e18afe5772caf6e69b7210ba6226870d513
Jordy Manner <jordy.manner@milkcreation.fr>
2019-01-25 21:00:19 +0000

    tag 2.0.54 -- Correctif bug pageHook

---
+++ src/Contracts/Partial/Modal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace tiFy\Contracts\Partial;
+
+interface Modal extends PartialController
+{
+    /**
+     * Affichage d'un lien de déclenchement de la modale.
+     *
+     * @param array $attrs Liste des attributs de configuration.
+     *
+     * @return string
+     */
+    public function trigger($attrs = []);
+
+    /**
+     * Chargement du contenu de la modale via Ajax.
+     *
+     * @return void
+     */
+    public function wp_ajax();
+}
