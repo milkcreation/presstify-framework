0c075dc8e586677ac04ed14f036c6f671030cc33
Jordy Manner <jordy.manner@milkcreation.fr>
2019-01-25 16:26:42 +0000

    tag 2.0.52 -- SelectJs ajout événement + class view PHP (artefact imcomplet)

---
+++ src/User/SignIn/views/lost-password-link.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @var tiFy\User\SignIn\SignInViewController $this
+ */
+?>
+
+<a  href="<?php echo \wp_lostpassword_url($this->get('lost_password_link.redirect', '')); ?>\"
+    title="<?php echo $this->get('lost_password_link.title', __('Récupération de mot de passe perdu', 'tify')); ?>"
+    class="tiFySignIn-LostPasswordLink"
+>
+    <?php echo $this->get('lost_password_link.content', ''); ?>
+</a>
