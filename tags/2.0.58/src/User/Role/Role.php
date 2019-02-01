c95cad08d475352f23496ece2824e9b12e91bd7f
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-02-01 08:22:07 +0000

    branch 2.0 -- Assets base_url

---
+++ tags/2.0.58/src/User/Role/Role.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @see https://codex.wordpress.org/Roles_and_Capabilities
+ */
+
+namespace tiFy\User\Role;
+
+use tiFy\Contracts\User\UserRoleItemController;
+use tiFy\User\Role\RoleController;
+
+class Role
+{
+    /**
+     * Liste des éléments déclarés.
+     * @var UserRoleItemController[]
+     */
+    protected $items = [];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        add_action(
+            'init',
+            function () {
+                foreach (config('user.role', []) as $name => $attrs) :
+                    $this->_register($name, $attrs);
+                endforeach;
+            },
+            0
+        );
+    }
+
+    /**
+     * Déclaration de rôle.
+     *
+     * @param string $name Nom de qualification.
+     * @param array $attrs Liste des attributs de configuration.
+     *
+     * @return $this
+     */
+    public function add($name, $attrs = [])
+    {
+        config()->set(
+            'user.role',
+            array_merge(
+                [$name =>$attrs],
+                config('user.role', [])
+            )
+        );
+
+        return $this;
+    }
+
+    /**
+     * Enregistrement d'un rôle déclaré.
+     *
+     * @param string $name Nom de qualification.
+     * @param array $attrs Liste des attributs de configuration.
+     *
+     * @return UserRoleItemController
+     */
+    private function _register($name, $attrs = [])
+    {
+        if (isset($this->items[$name])) :
+            return $this->items[$name];
+        endif;
+
+        return $this->items[$name] = new RoleItemController($name, $attrs);
+    }
+
+    /**
+     * Récupération d'une instance de rôle déclaré.
+     *
+     * @param string $name Nom de qualification du rôle.
+     *
+     * @return null|UserRoleItemController
+     */
+    public function get($name)
+    {
+        return isset($this->items[$name])
+            ? $this->items[$name]
+            : null;
+    }
+}
