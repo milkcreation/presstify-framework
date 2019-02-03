21754abaa989214fb1b879ac95a78f76825e0a81
Jordy Manner <jordy.manner@milkcreation.fr>
2019-02-03 19:32:40 +0000

    tag 2.0.61 -- Réécriture cron

--- src/Kernel/Container/Container.php
+++ src/Kernel/Container/Container.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Collection;
 use League\Container\Container as LeagueContainer;
 use League\Container\ReflectionContainer;
 use League\Container\ServiceProvider\ServiceProviderInterface;
+use tiFy\Contracts\Container\ContainerInterface;
 
 class Container extends LeagueContainer implements ContainerInterface
 {
     /**
      * Liste des services déclarés.
-     * @var ServiceInterface[]
+     * @var Service[]
      */
-    protected $items = [];
-
-    /**
-     * Liste des fournisseurs de service.
-     * @var string[]
-     */
-    protected $serviceProviders = [];
+    protected static $items = [];
 
     /**
      * Liste des alias de résolution de services.
@@ -32,6 +27,12 @@ class Container extends LeagueContainer implements ContainerInterface
      * @see http://container.thephpleague.com/2.x/auto-wiring/
      */
     protected $autoWiring = false;
+
+    /**
+     * Liste des fournisseurs de service.
+     * @var string[]
+     */
+    protected $serviceProviders = [];
 
     /**
      * CONSTRUCTEUR.
@@ -47,13 +48,14 @@ class Container extends LeagueContainer implements ContainerInterface
         endif;
 
         foreach ($this->getServiceProviders() as $serviceProvider) :
-            $this->share($serviceProvider)->withArgument($this);
-            $concrete = $this->get($serviceProvider);
+            $resolved = $this->singleton($serviceProvider)
+                ->build([$this]);
 
-            if ($concrete instanceof ServiceProvider) :
-                $this->addServiceProvider($concrete);
+            if ($resolved instanceof ServiceProviderInterface) :
+                $this->addServiceProvider($resolved);
             endif;
         endforeach;
+
     }
 
     /**
@@ -69,7 +71,7 @@ class Container extends LeagueContainer implements ContainerInterface
      */
     public function bound($abstract)
     {
-        return isset($this->items[$this->getAbstract($abstract)]);
+        return isset(self::$items[$this->getAbstract($abstract)]) || $this->has($abstract);
     }
 
     /**
@@ -81,19 +83,27 @@ class Container extends LeagueContainer implements ContainerInterface
             $concrete = $abstract;
         endif;
 
-        $alias = $this->getAlias($concrete);
+        $alias = $this->getAlias($abstract);
 
-        return $this->items[$abstract] = $this->addService($abstract, compact('alias', 'concrete', 'singleton'));
+        return self::$items[$abstract] = $this->addService($abstract, compact('alias', 'concrete', 'singleton'));
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getAlias($concrete)
+    public function get($id, array $args = [])
     {
-        $alias = array_search($concrete, $this->getAliases());
+        return $this->resolve($id, $args) ? : parent::get($id, $args);
+    }
 
-        return $alias !== false ? $alias : $concrete;
+    /**
+     * {@inheritdoc}
+     */
+    public function getAlias($abstract)
+    {
+        $alias = array_search($abstract, $this->getAliases());
+
+        return $alias !== false ? $alias : $abstract;
     }
 
     /**
@@ -109,8 +119,10 @@ class Container extends LeagueContainer implements ContainerInterface
      */
     public function getAbstract($alias)
     {
+        $items = self::$items;
+
         return (
-            $exists = (new Collection($this->items))->first(function($item) use ($alias) {
+            $exists = (new Collection($items))->first(function (Service $item) use ($alias) {
                 return $item->getAlias() === $alias;
             })
         )
@@ -124,8 +136,9 @@ class Container extends LeagueContainer implements ContainerInterface
     public function getService($abstract)
     {
         $abstract = $this->getAbstract($abstract);
-        if (isset($this->items[$abstract])) :
-            return $this->items[$abstract];
+
+        if (isset(self::$items[$abstract])) :
+            return self::$items[$abstract];
         else :
             throw new \InvalidArgumentException(
                 sprintf('(%s) n\'est pas distribué par le fournisseur de service.', $abstract),
@@ -145,15 +158,25 @@ class Container extends LeagueContainer implements ContainerInterface
     /**
      * {@inheritdoc}
      */
-    public function resolve($abstract, $args = [])
+    public function resolve($alias, $args = [])
     {
         try {
-            $service = $this->getService($abstract);
-        } catch (\InvalidArgumentException $e) {
-            return \wp_die($e->getMessage(), '', 501);
-        }
+            $resolved = $this->getService($alias);
 
-        return $service->build($args);
+            return $resolved->build($args);
+        } catch (\InvalidArgumentException $e) {
+            return null;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAlias($alias, $concrete)
+    {
+        $this->aliases[$alias] = $concrete;
+
+        return $this;
     }
 
     /**
