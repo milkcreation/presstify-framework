c49785269b6a3d63fe38f446e857598a405e09ca
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-02-05 16:40:24 +0000

    branch2.0 -- Ajout de Mustache + CollectJs aux deps NPM

--- branches/2.0/src/tiFy.php
+++ branches/2.0/src/tiFy.php
@@ -14,7 +14,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.61
+ * @version 2.0.62
  * @copyright Milkcreation
  */
 final class tiFy extends Container
