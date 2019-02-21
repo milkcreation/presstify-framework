8af9665e9b2b3b9f27728fc865cda667a8dc2abc
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-02-21 15:05:00 +0000

    tag 2.0.69 -- bug Filesystem config vide

---
+++ tags/2.0.69/assets/partial/modal/index.js
@@ -0,0 +1,9 @@
+/** DEPS */
+/** Bootstrap - Modal */
+import 'bootstrap/js/dist/modal';
+
+/** SASS */
+import './scss/styles.scss';
+
+/** JS */
+import './js/scripts';
---
+++ tags/2.0.69/src/Wp/Filesystem/Filesystem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tiFy\Wp\Filesystem;
+
+use tiFy\Contracts\Filesystem\StorageManager;
+
+class Filesystem
+{
+    /**
+     * Instance du controleur du gestion de systèmes de fichier.
+     * @var StorageManager
+     */
+    protected $manager;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param StorageManager $manager Instance du controleur du gestion de systèmes de fichier.
+     *
+     * @return void
+     */
+    public function __construct(StorageManager $manager)
+    {
+        $this->manager = $manager;
+
+        foreach(config('filesystem', []) as $name => $attrs) :
+            $this->manager->register($name, $attrs);
+        endforeach;
+    }
+}
