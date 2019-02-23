b0361888486efbf96233b0f6cd9d2c36aa100c46
Jordy Manner <jordy.manner@milkcreation.fr>
2019-02-23 09:46:16 +0000

    tag 2.0.70 -- SelectJs : event change - Console : Controller phase #1 - Mailer : debug retour erreurs

---
+++ assets/partial/modal/js/scripts.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function ($) {
+    $('[aria-control="modal"]')
+        .modal()
+        .on('shown.bs.modal', function () {
+            var $modal = $(this);
+            var o = $.parseJSON(decodeURIComponent($modal.data('options')));
+
+            if (tify[o.id] === undefined){
+                tify[o.id] = {};
+            }
+
+            if ($('.modal-content', $modal).length) {
+                tify[o.id].original = $('.modal-content', $modal).html();
+            }
+
+            if (o.ajax) {
+                if (tify[o.id].content === undefined) {
+                    tify[o.id].content = resp;
+                    $.post(tify.ajax_url, o.ajax, function(resp) {$('.modal-content', $modal).$modal.html(resp);});
+                } else {
+                    $('.modal-content', $modal).html(tify[o.id].content);
+                }
+            }
+        })
+        .on('hidden.bs.modal', function (){
+            var $modal = $(this);
+            var o = $.parseJSON(decodeURIComponent($modal.data('options')));
+
+            if (o.ajax) {
+                if (tify[o.id].original !== undefined) {
+                    $('.modal-content', $modal).html(tify[o.id].original);
+                } else {
+                    $('.modal-content', $modal).empty();
+                }
+            }
+        });
+
+    $(document).on('click', '[aria-control="modal-trigger"]', function (e) {
+        e.preventDefault();
+
+        $($(this).data('target') + '[aria-control="modal"]').modal('show');
+    });
+});
--- src/Components/Partial/HolderImage/templates/holder-image.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var tiFy\Partial\TemplateController $this
- */
-?>
-<div <?php echo $_attrs; ?>>
-    <span class="tiFyPartial-HolderImageSizer" style="padding-top:<?php echo ceil((100/$width)* $height); ?>%"></span>
-    <div class="tiFyPartial-HolderImageContent"><?php echo $content; ?></div>
-</div>
