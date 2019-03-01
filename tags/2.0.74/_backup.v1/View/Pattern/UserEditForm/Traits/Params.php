b00de50c7d64648cb2dc69dc78de49d4a0b70926
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-03-01 10:11:08 +0000

    tag 2.0.74 -- Support\Str + Support\Collection + Support\Arr && deprecated Kernel\Collection\* && Réécriture Wp\Query\*Post

---
+++ tags/2.0.74/_backup.v1/View/Pattern/UserEditForm/Traits/Params.php
@@ -0,0 +1,45 @@
+<?php 
+namespace tiFy\Core\Ui\Admin\Templates\UserEditForm\Traits;
+
+trait Params
+{
+    /**
+     * Définition de la liste des roles à afficher
+     *
+     * @param array $roles Liste des roles à afficher définis en paramètre
+     *
+     * @return array
+     */
+    public function set_param_roles($roles = [])
+    {
+        return $roles;
+    }
+
+    /**
+     * Initialisation de la liste des roles à afficher
+     *
+     * @param array $roles Liste des roles à afficher existants
+     *
+     * @return array
+     */
+    public function init_param_roles($roles = [])
+    {
+        if ($editable_roles = array_reverse(\get_editable_roles())) :
+            $editable_roles = array_keys($editable_roles);
+        endif;
+
+        $_roles = [];
+        if ($roles) :
+            foreach ($roles as $role) :
+                if (!in_array($role, $editable_roles)) :
+                    continue;
+                endif;
+                array_push($_roles, $role);
+            endforeach;
+        else :
+            $_roles = $editable_roles;
+        endif;
+
+        return $_roles;
+    }
+}
