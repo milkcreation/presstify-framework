3805140e9829e032c9360a69836bf187b4e23e42
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-03-04 09:26:05 +0000

    tag 2.0.76 -- Correctif bug Support\DateTime

---
+++ tags/2.0.76/src/Wp/Query/QueryTerms.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace tiFy\Wp\Query;
+
+use tiFy\Taxonomy\Query\TermQueryCollection;
+use WP_Term;
+use WP_Term_Query;
+
+class QueryTerms extends TermQueryCollection
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param null|WP_Term_Query $wp_term_query Requête Wordpress de récupération de termes.
+     *
+     * @return void
+     */
+    public function __construct(WP_Term_Query $wp_term_query)
+    {
+        parent::__construct($wp_term_query instanceof WP_Term_Query ? $wp_term_query->terms : []);
+    }
+
+    /**
+     * Récupération d'une instance basée sur la liste des arguments.
+     * @see https://developer.wordpress.org/reference/classes/wp_term_query/
+     *
+     * @param array $args Liste des arguments de la requête récupération des éléments
+     *
+     * @return static
+     */
+    public static function createFromArgs($args = [])
+    {
+        return new static(new WP_Term_Query($args));
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param WP_Term $item Objet terme Wordpress.
+     *
+     * @return void
+     */
+    public function wrap($item, $key = null)
+    {
+        $this->items[$key] = app()->get('wp.query.term', [$item]);
+    }
+}
