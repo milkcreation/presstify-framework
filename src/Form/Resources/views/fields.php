90007e88f1809045f5c365a59519b29e3ca7fc73
Jordy Manner <jordy.manner@milkcreation.fr>
2019-03-08 15:42:24 +0000

    tag 2.0.79 -- Bug animate MacOS && new Support\Collection + Support\ParamsBag + Wp\QueryTerm(s)

---
+++ src/Form/Resources/views/fields.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Liste des champs du formulaire.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Contracts\Form\FactoryView $this
+ * @var tiFy\Contracts\Form\FactoryFields|tiFy\Contracts\Form\FactoryField[] $fields
+ */
+?>
+
+<?php if ($fields) : ?>
+    <div class="Form-fields">
+        <?php if ($fields->hasGroup()) : ?>
+            <?php foreach ($fields->byGroup() as $num => $groupFields) : ?>
+                <div class="Form-fieldsGroup Form-fieldsGroup--<?php echo $num; ?>">
+                    <?php foreach ($groupFields as $field) : ?>
+                        <?php $this->insert('field', compact('field')); ?>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+        <?php else : ?>
+            <?php foreach ($fields as $field) : ?>
+                <?php $this->insert('field', compact('field')); ?>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
+<?php endif; ?>
---
+++ src/Template/Resources/views/list-table/tablenav.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Interface de navigation de la table.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\ListTable\Viewer\Viewer $this
+ * @var string $which top|bottom.
+ */
+?>
+<div class="tablenav <?php echo esc_attr($which); ?>">
+
+    <?php $this->insert('bulk-actions', compact('which')); ?>
+
+    <?php $this->insert('pagination', compact('which')); ?>
+
+    <br class="clear" />
+</div>
