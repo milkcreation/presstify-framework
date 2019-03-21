76ff4a7857b98042da1c9390e985aac6ff736b9a
Jordy Manner <jordy.manner@milkcreation.fr>
2019-03-21 15:49:45 +0000

    tag 2.0.90 -- Ajustement réécriture User

---
+++ src/Template/Templates/ListTable/Columns/ColumnsItemCb.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace tiFy\Template\Templates\ListTable\Columns;
+
+use tiFy\Support\HtmlAttrs;
+
+class ColumnsItemCb extends ColumnsItem
+{
+    /**
+     * Indice de l'entête courante.
+     * @var int
+     */
+    protected static $headerIndex = 0;
+
+    /**
+     * @inheritdoc
+     */
+    public function header($with_id = true)
+    {
+        $classes = ['manage-column', "column-{$this->getName()}", 'check-column'];
+
+        if ($this->isHidden()) :
+            $classes[] = 'hidden';
+        endif;
+
+        $attrs = [];
+        if ($with_id) :
+            $attrs['id'] = $this->getName();
+        endif;
+
+        $attrs['class'] = join(' ', $classes);
+
+        return $this->template->viewer('thead-col_cb', [
+            'attrs' => HtmlAttrs::createFromAttrs($attrs),
+            'index' => ++self::$headerIndex,
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isPrimary()
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function parse($attrs = [])
+    {
+        parent::parse($attrs);
+
+        $this->pull('attrs.data-colname');
+
+        $this->set('attrs.class', 'check-column');
+
+        $this->set('attrs.scope', 'row');
+    }
+}
