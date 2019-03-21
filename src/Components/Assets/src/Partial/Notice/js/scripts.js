89b46a90e48450caa1e9a422c8505c8ea1bcd98a
Jordy Manner <jordy.manner@milkcreation.fr>
2019-03-21 16:24:00 +0000

    shop.pixvert.fr -- tag 2.0.91 -- Correctif résolution container get > rawArg

--- src/Components/Assets/src/Partial/Notice/js/scripts.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    $(document).on('click', '[aria-control="notice"] [aria-toggle="dismiss"]', function(e){
-        e.preventDefault();
-
-        $(this).closest('[aria-control="notice"]').attr('aria-hide', 'true');
-    });
-});
