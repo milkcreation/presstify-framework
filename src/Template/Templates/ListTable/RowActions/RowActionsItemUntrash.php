a3b81130033b1b2d34f309e93e3ea88e2dbebd42
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-09 13:24:46 +0000

    tag 2.0.102 -- correctifs maj env pixvert

---
+++ src/Template/Templates/ListTable/RowActions/RowActionsItemUntrash.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\ListTable\RowActions;
+
+class RowActionsItemUntrash extends RowActionsItem
+{
+    /**
+     * @inheritdoc
+     */
+    public function defaults()
+    {
+        return [
+            'content'   => __('Rétablir', 'tify'),
+            'title'     => __('Restauration de l\'élément', 'tify'),
+            'nonce'     => $this->getNonce(),
+            'referer' => true
+        ];
+    }
+}
