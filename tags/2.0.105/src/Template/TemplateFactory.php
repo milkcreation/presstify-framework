28980e5fc50d8e16f65c54d0fc54891e4972a1ca
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-04-12 16:25:22 +0000

    tag 2.0.105 -- Correctif réécriture + injection de dépendances

---
+++ tags/2.0.105/src/Template/TemplateFactory.php
@@ -0,0 +1,308 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template;
+
+use tiFy\Contracts\Container\Container;
+use tiFy\Contracts\Container\ServiceProvider;
+use tiFy\Contracts\Template\FactoryAssets;
+use tiFy\Contracts\Template\FactoryNotices;
+use tiFy\Contracts\Template\FactoryRequest;
+use tiFy\Contracts\Template\FactoryUrl;
+use tiFy\Contracts\Template\TemplateFactory as TemplateFactoryContract;
+use tiFy\Support\ParamsBag;
+use tiFy\Template\Factory\FactoryServiceProvider;
+use tiFy\Support\Str;
+use tiFy\tiFy;
+
+class TemplateFactory implements TemplateFactoryContract
+{
+    /**
+     * Indicateur de démarrage du controleur.
+     * @var boolean
+     */
+    private $booted = false;
+
+    /**
+     * Instance du controleur de traitement des attributs de configuration.
+     * @var ParamsBag
+     */
+    protected $config;
+
+    /**
+     * Indicateur de chargement des ressources.
+     * @var boolean
+     */
+    protected $loaded = false;
+
+    /**
+     * Nom de qualification.
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * Identifiant de qualification compatible au formatage dans une url.
+     * @var string
+     */
+    protected $slug;
+
+    /**
+     * Liste des fournisseurs de service.
+     * @var string[]
+     */
+    protected $serviceProviders = [
+        FactoryServiceProvider::class
+    ];
+
+    /**
+     * CONSTRUCTEUR
+     *
+     * @param array $attrs Liste des attributs de configuration personnalisés.
+     *
+     * @return void
+     */
+    public function __construct(?array $attrs = [])
+    {
+        $this->config = (new ParamsBag())->set($attrs);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __invoke(string $name): TemplateFactoryContract
+    {
+        if (!$this->booted) {
+            $this->name = $name;
+            foreach ($this->getServiceProviders() as $serviceProvider) {
+                $this->getContainer()->share(
+                    "template.factory.service-provider.{$this->name}",
+                    $resolved = new $serviceProvider($this)
+                );
+
+                if ($resolved instanceof ServiceProvider) {
+                    $resolved->setContainer($this->getContainer());
+                    $this->getContainer()->addServiceProvider($resolved);
+                }
+            }
+
+            events()->trigger('template.factory.boot.' . $this->name, [&$this]);
+
+            $this->boot();
+
+            $this->booted = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function __toString(): string
+    {
+        $this->load();
+
+        return (string)$this->render();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function assets(): FactoryAssets
+    {
+        return $this->resolve('assets');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function boot(): TemplateFactoryContract
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function config($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return $this->config;
+        } elseif(is_array($key)) {
+            return $this->config->set($key);
+        } else {
+            return $this->config->get($key, $default);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function db()
+    {
+        return $this->resolve('db');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function display()
+    {
+        echo (string)$this->render();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getContainer(): Container
+    {
+        return tiFy::instance();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getServiceProviders()
+    {
+        return $this->serviceProviders;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function bound(string $abstract)
+    {
+        return $this->getContainer()->has("template.factory.{$this->name()}.{$abstract}");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function label(?string $key = null, string $default = '')
+    {
+        $labels = $this->resolve('labels');
+
+        return is_null($key) ? $labels : $labels->get($key, $default);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function load(): TemplateFactoryContract
+    {
+        if (!$this->loaded) {
+            $this->loaded = true;
+            $this->process();
+            $this->prepare();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function notices(): FactoryNotices
+    {
+        return $this->resolve('notices');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function param($key = null, $default = null)
+    {
+        $params = $this->resolve('params');
+
+        if (is_null($key)) {
+            return $params;
+        } elseif (is_array($key)) {
+            return $params->set($key);
+        } else {
+            return $params->get($key, $default);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function prepare(): TemplateFactoryContract
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function process(): TemplateFactoryContract
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function render()
+    {
+        return ($this->config()->has('content'))
+            ? call_user_func_array($this->config('content'), [&$this])
+            : __('Aucun contenu à afficher', 'tify');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function request(): FactoryRequest
+    {
+        return $this->resolve('request');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(string $abstract, array $args = [])
+    {
+        return $this->getContainer()->get("template.factory.{$this->name()}.{$abstract}", $args);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function slug(): string
+    {
+        if (is_null($this->slug)) {
+            $this->slug = Str::slug($this->name);
+        }
+        return $this->slug;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function url(): FactoryUrl
+    {
+        return $this->resolve('url');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function viewer(?string $view = null, array $data = [])
+    {
+        $viewer = $this->resolve('viewer');
+
+        if (func_num_args() === 0) {
+            return $viewer;
+        }
+
+        return $viewer->make("_override::{$view}", $data);
+    }
+}
---
+++ tags/2.0.105/src/Template/Templates/ListTable/Columns/ColumnsCollection.php
@@ -0,0 +1,145 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\ListTable\Columns;
+
+use tiFy\Support\Collection;
+use tiFy\Template\Templates\ListTable\Contracts\ColumnsCollection as ColumnsCollectionContract;
+use tiFy\Template\Templates\ListTable\Contracts\ColumnsItem;
+use tiFy\Template\Templates\ListTable\Contracts\ListTable;
+
+class ColumnsCollection extends Collection implements ColumnsCollectionContract
+{
+    /**
+     * Instance du gabarit associé.
+     * @var ListTable
+     */
+    protected $factory;
+
+    /**
+     * Liste des colonnes.
+     * @var ColumnsItem[]
+     */
+    protected $items = [];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param array $items Liste des éléments
+     * @param ListTable $factory Instance du motif d'affichage associé.
+     *
+     * @return void
+     */
+    public function __construct(array $items, ListTable $factory)
+    {
+        $this->factory = $factory;
+
+        $this->parse($items);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function countVisible(): int
+    {
+        return count($this->getVisible());
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getHideable(): iterable
+    {
+        return $this->collect()
+            ->filter(function (ColumnsItem $item) {
+                return $item->get('hideable') === true;
+            });
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getHidden(): array
+    {
+        return $this->collect()
+            ->filter(function (ColumnsItem $item) {
+                return $item->isHidden();
+            })
+            ->pluck('name', null)
+            ->all();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getPrimary(): string
+    {
+        if (
+            ($column_primary = $this->factory->param('column_primary', '')) &&
+            ($column_primary !== 'cb') &&
+            $this->has($column_primary)
+        ) {
+            return (string)$column_primary;
+        } else {
+            return $this->collect()->first(function (ColumnsItem $item) {
+                return $item->getName() !== 'cb';
+            })->getName();
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSortable(): array
+    {
+        return $this->collect()
+            ->filter(function (ColumnsItem $item) {
+                return $item->isSortable();
+            })
+            ->pluck('sortable', 'name')
+            ->all();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getVisible(): array
+    {
+        return $this->collect()
+            ->filter(function (ColumnsItem $item) {
+                return !$item->isHidden();
+            })
+            ->pluck('name', null)
+            ->all();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function parse(array $columns = []): ColumnsCollectionContract
+    {
+        foreach ($columns as $name => $attrs) {
+            if (is_numeric($name)) {
+                $name = $attrs;
+                $attrs = [];
+            } elseif (is_string($attrs)) {
+                $attrs = ['title' => $attrs];
+            }
+
+            $alias = $this->factory->bound("columns.item.{$name}")
+                ? "columns.item.{$name}"
+                : 'columns.item';
+
+            $this->items[$name] = $this->factory->resolve($alias, [$name, $attrs, $this->factory]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function renderToggle(): string
+    {
+        return ($cols = $this->getHideable()) ? (string)$this->factory->viewer('columns-toggle', compact('cols')) : '';
+    }
+}
