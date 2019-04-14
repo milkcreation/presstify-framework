94a386fc072612bde40e8a1b530c50b354817725
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-14 11:53:46 +0000

    tag 2.0.106 -- correctif compatibilité composer

---
+++ src/Contracts/Partial/CookieNotice.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace tiFy\Contracts\Partial;
+
+interface CookieNotice extends PartialFactory
+{
+    /**
+     * Récupération d'un cookie.
+     *
+     * @return string
+     */
+    public function getCookie();
+
+    /**
+     * Définition d'un cookie.
+     *
+     * @var string $name Nom de qualification du cookie.
+     * @var int $cookie_expire Temps avant l'expiration du cookie. Exprimé en secondes.
+     *
+     * @return void
+     */
+    public function setCookie($cookie_name, $cookie_expire = 0);
+
+    /**
+     * Génération du cookie de notification via Ajax.
+     *
+     * @return void
+     */
+    public function wp_ajax();
+}
