8dba0e0c81a215711fde5504b8418f35b6fb2c95
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-04-14 17:17:12 +0000

    tag 2.0.107 -- Correctif maj Pollen + wp-classic svn stat APP_URL

---
+++ tags/2.0.107/assets/partial/_image-lightbox/tiFyImageLightbox/index.js
@@ -0,0 +1,7 @@
+/** Sass **/
+import './scss/styles.scss';
+
+/** Scripts **/
+import 'jquery-ui/ui/widget.js';
+import './js/imageLightbox';
+import './js/scripts';
---
+++ tags/2.0.107/assets/partial/spinner/index.js
@@ -0,0 +1,2 @@
+/** CSS */
+import "./css/spinkit.min.css";
