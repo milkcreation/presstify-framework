92c55af14bf1d0917470f59297ed6b1179740fc2
Jordy Manner <jordy.manner@milkcreation.fr>
2019-04-15 09:15:47 +0000

    tag 2.0.109 -- maj TaxonomySelector

---
+++ src/Contracts/User/SigninFactory.php
@@ -0,0 +1,132 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Contracts\User;
+
+use tiFy\Contracts\Support\ParamsBag;
+use tiFy\Contracts\Kernel\Notices;
+
+interface SigninFactory extends ParamsBag
+{
+    /**
+     * Résolution de sortie de la classe en tant que chaîne de caractère.
+     * {@internal Affichage du formulaire d'authentification}
+     *
+     * @return string
+     */
+    public function __toString();
+
+    /**
+     * Ajout d'un message de notification.
+     *
+     * @param string $type Type de notification. error|info.
+     * @param string $message Intitulé du message de notification.
+     * @param string $code Identifiant de qualification du message.
+     * @param array $datas Liste des données associées.
+     *
+     * @return static
+     */
+    public function addNotice($type, $message = '', $code = null, $datas = []): SigninFactory;
+
+    /**
+     * Affichage du formulaire d'authentification.
+     *
+     * @return string
+     */
+    public function authForm();
+
+    /**
+     * Initialisation du formulaire d'authentification.
+     *
+     * @return void
+     */
+    public function boot();
+
+    /**
+     * Récupération de l'url de redirection du formulaire d'authentification.
+     *
+     * @param string|null $redirect_url Url de redirection personnalisée.
+     *
+     * @return string
+     */
+    public function getAuthRedirectUrl(?string $redirect_url = null): string;
+
+    /**
+     * Récupération de l'url de redirection du formulaire d'authentification.
+     *
+     * @param string|null Url de redirection personnalisée.
+     *
+     * @return string
+     */
+    public function getLogoutRedirectUrl(?string $redirect_url = null): string;
+
+    /**
+     * Récupération de l'url de déconnection.
+     *
+     * @param string|null Url de redirection personnalisée.
+     *
+     * @return string
+     */
+    public function getLogoutUrl(?string $redirect_url = null): string;
+
+    /**
+     * Récupération de la liste des messages de notification selon leur type.
+     *
+     * @param string $type Type de message. error|info.
+     *
+     * @return array
+     */
+    public function getMessages(string $type): array;
+
+    /**
+     * Récupération du nom de qualification du controleur.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Récupération de la liste des rôles autorisés à se connecter depuis l'interface de login.
+     *
+     * @return array
+     */
+    public function getRoles(): array;
+
+    /**
+     * Traitement.
+     *
+     * @return void
+     */
+    public function handle();
+
+    /**
+     * Vérification d'autorisation de connection de rôle(s) utilisateur donné(s).
+     *
+     * @param string|array $role Nom de qualification ou liste des nom de qualification des roles à vérifier.
+     *
+     * @return boolean
+     */
+    public function hasRole($role): bool;
+
+    /**
+     * Affichage du lien de déconnection.
+     *
+     * @param array $attrs Liste des attributs de personnalisation.
+     *
+     * @return string
+     */
+    public function logoutLink($attrs = []);
+
+    /**
+     * Affichage du lien vers l'interface de récupération de mot de passe oublié.
+     *
+     * @return string
+     */
+    public function lostpasswordLink();
+
+    /**
+     * Récupération de l'instance du gestionnaire de message de notification.
+     *
+     * @return Notices
+     */
+    public function notices(): Notices;
+}
