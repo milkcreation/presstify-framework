b5f9d915f0f80aa704491f4b723bf5f7d51b838c
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-04-24 08:25:58 +0000

    tag 2.0.116 -- correctif erreur 404 routing wp

---
+++ tags/2.0.116/src/Template/Resources/views/list-table/columns-toggle.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Interface de bascule d'affichage des colonnes de la table.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\ListTable\Viewer\Viewer $this
+ * @var tiFy\Template\Templates\ListTable\Contracts\ColumnsCollection $cols
+ * @var tiFy\Template\Templates\ListTable\Contracts\ColumnsItem $col
+ */
+?>
+<?php foreach ($this->columns()->getHideable() as $col) {
+    echo field('checkbox', [
+        'after' => (string)field('label', [
+            'content' => $col->getTitle(),
+            'attrs'   => [
+                'for' => 'ListTable-columnToggle--' . $col->getName()
+            ]
+        ]),
+        'value' => $col->getName(),
+        'attrs' => [
+            'id'           => 'ListTable-columnToggle--' . $col->getName(),
+            'data-control' => 'list-table.column.toggle'
+        ],
+        'checked' => true
+    ]);
+}
