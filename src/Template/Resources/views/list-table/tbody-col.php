608f7d0858b163f685e2d3c509c68e4ec17ef265
Jordy Manner <jordy.manner@milkcreation.fr>
2019-05-06 13:19:07 +0000

    branch 2.0 -- Template\ListTable -- Séparation td <> content pour récupération attrs td via ajax

--- src/Template/Resources/views/list-table/tbody-col.php
+++ src/Template/Resources/views/list-table/tbody-col.php
@@ -3,10 +3,9 @@
  * Colonne par défaut d'une ligne de données de la table.
  * ---------------------------------------------------------------------------------------------------------------------
  * @var tiFy\Template\Templates\ListTable\Viewer\Viewer $this
- * @var tiFy\Template\Templates\ListTable\Contracts\Item $item
  * @var tiFy\Template\Templates\ListTable\Contracts\ColumnsItem $column
+ * @var tiFy\Template\Templates\ListTable\Contracts\Item|WP_Post $item
+ * @var string $content
  */
 ?>
-<td <?php echo $this->get('attrs', ''); ?>>
-    <?php echo $this->get('content', ''); ?>
-</td>
+<?php echo $this->get('content', ''); ?>
--- src/Template/Resources/views/list-table/tbody-col_cb.php
+++ src/Template/Resources/views/list-table/tbody-col_cb.php
@@ -3,14 +3,13 @@
  * Colonne "Case à coché" de la ligne de données de la table.
  * ---------------------------------------------------------------------------------------------------------------------
  * @var tiFy\Template\Templates\ListTable\Viewer\Viewer $this
- * @var tiFy\Template\Templates\ListTable\Contracts\Item $item
  * @var tiFy\Template\Templates\ListTable\Contracts\ColumnsItem $column
+ * @var tiFy\Template\Templates\ListTable\Contracts\Item|WP_Post $item
+ * @var string $content
  */
 ?>
-<th <?php echo $this->get('attrs', ''); ?>>
-    <input
-        type="checkbox"
-        name="<?php echo $item->getPrimaryKey(); ?>[]"
-        value="<?php echo $item->getPrimary(); ?>"
-    />
-</th>
+<input
+    type="checkbox"
+    name="<?php echo $item->getPrimaryKey(); ?>[]"
+    value="<?php echo $item->getPrimary(); ?>"
+/>
