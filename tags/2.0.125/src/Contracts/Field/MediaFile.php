2b8e549f569cdc91a40ab6a29dfa228adc408de9
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-05-08 08:29:05 +0000

    tag 2.0.125 -- évolution Form && correctif + Validator mais deprecated déjà svn stat

---
+++ tags/2.0.125/src/Contracts/Field/MediaFile.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace tiFy\Contracts\Field;
+
+interface MediaFile extends FieldController
+{
+
+}
---
+++ tags/2.0.125/src/Wordpress/Database/Model/Term.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Model;
+
+use Corcel\Model\Term as CorcelTerm;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * Class Term
+ * @package tiFy\Wordpress\Database\Model
+ *
+ * @mixin Builder
+ */
+class Term extends CorcelTerm
+{
+
+}
