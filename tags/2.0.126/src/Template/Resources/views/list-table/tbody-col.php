6b55b77b6fed37f52ee1c1dc56b45ff28e8bf2c0
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-05-08 17:11:21 +0000

    tag 2.0.126 -- FormFactory auto + Correctif Addon\Mailer

---
+++ tags/2.0.126/src/Template/Resources/views/list-table/tbody-col.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Colonne par défaut d'une ligne de données de la table.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\ListTable\Viewer\Viewer $this
+ * @var tiFy\Template\Templates\ListTable\Contracts\ColumnsItem $column
+ * @var tiFy\Template\Templates\ListTable\Contracts\Item|WP_Post $item
+ * @var string $content
+ */
+?>
+<?php echo $this->get('content', ''); ?>
