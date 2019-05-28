11e6cd15f58f4469ae5c426d12f95443c3c21282
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-05-28 13:45:32 +0000

    branch 2.0 -- évolution Filesystem

--- branches/2.0/src/tiFy.php
+++ branches/2.0/src/tiFy.php
@@ -12,7 +12,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.135
+ * @version 2.0.136
  * @copyright Milkcreation
  */
 final class tiFy extends Container
