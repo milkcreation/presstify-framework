ca42e4bc65715c975a2829d16ed8e857d1926705
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-06-06 17:17:01 +0000

    branch 2.0 -- Correctif QueryPost+QueryUser > save meta maybe_serialize

--- branches/2.0/src/tiFy.php
+++ branches/2.0/src/tiFy.php
@@ -12,7 +12,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.141
+ * @version 2.0.142
  * @copyright Milkcreation
  */
 final class tiFy extends Container
