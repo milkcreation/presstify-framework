7b446d7a065002ddcdc9388e8614b834782a57bd
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-06-06 17:18:06 +0000

    tag 2.0.142 -- Correctif QueryPost+QueryUser > save meta maybe_serialize

---
+++ tags/2.0.142/_backup.v1/Tools/Video/Inline.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($){
+	$(document).on('click', '.tiFyVideo-inlineToggle', function(e) {
+		e.preventDefault();
+		var $this = $(this),
+			$target = $($this.data('target')),
+			video = $this.data('video');
+		if(!$target.length) {
+			return false;
+		}
+		if($this.hasClass('tiFyVideo-inlineToggle--loading')){
+			return false;
+		}
+		$this.addClass('tiFyVideo-inlineToggle--loading');
+		$target.addClass('tiFyVideo-inlineViewer--loading');
+		$this.trigger('tify_video_inline_toggle.click', [$target, video]);
+		$.post( 
+			tify_ajaxurl, 
+			{ action : 'tiFyVideoGetEmbed', 'attr' : video }, 
+			function( resp ){
+				$this.removeClass('tiFyVideo-inlineToggle--loading');
+				$target.removeClass('tiFyVideo-inlineViewer--loading');
+				$target.html(resp);
+				$this.trigger('tify_video_inline_toggle.done', $target);
+			}
+		);
+	});
+});
