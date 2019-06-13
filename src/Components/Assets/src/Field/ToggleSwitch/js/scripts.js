4e9154d45d4b16ecb17b8b480eb80c334e805685
Jordy Manner <jordy.manner@milkcreation.fr>
2019-06-13 09:06:55 +0000

    tag 1.4.50b -- version lacentralmedicale

--- src/Components/Assets/src/Field/ToggleSwitch/js/scripts.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function ($) {
-    $(document).on('change', '.tiFyField-toggleSwitchRadio', function (e) {
-        $(this)
-            .closest('.tiFyField-toggleSwitch')
-            .trigger('tify_field.toggleSwitch.change', $(this).val());
-    });
-});
