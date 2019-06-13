73b18515f9177dd7b54e96a3ed78a6a510fcb39f
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-06-13 15:00:10 +0000

    tag 2.0.148 -- correctifs findPosts + mediaFile + mediaImage

---
+++ tags/2.0.148/assets/field/file-js/index.js
@@ -0,0 +1,12 @@
+// DEPS
+// jQuery-ui - Widget
+import 'jquery-ui/ui/widget';
+
+// dropzone
+
+
+// CSS
+import './scss/styles.scss';
+
+// JS
+import './js/scripts';
---
+++ tags/2.0.148/assets/field/media-file/js/scripts.js
@@ -0,0 +1,36 @@
+/* global wp */
+
+"use strict";
+
+let tiFyFieldMediaFileFrame;
+
+jQuery(document).ready(function ($) {
+    $(document).on('click', '[aria-control="media_file"]', function (e) {
+        e.preventDefault();
+
+        let $closest = $(this),
+            o = $.parseJSON(decodeURIComponent($(this).data('options')));
+
+        tiFyFieldMediaFileFrame = wp.media.frames.file_frame = wp.media(o);
+
+        tiFyFieldMediaFileFrame.on('select', function () {
+            let attachment = tiFyFieldMediaFileFrame.state().get('selection').first().toJSON();
+            $closest.attr('aria-active', 'true');
+            $('[aria-control="infos"]', $closest).val(attachment.title + ' → ' + attachment.filename);
+            $('[aria-control="input"]', $closest).val(attachment.id);
+        });
+
+        tiFyFieldMediaFileFrame.open();
+    });
+
+    $(document).on('click', '[aria-control="media_file"] [aria-control="reset"]', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        let $closest = $(this).parent();
+
+        $closest.attr('aria-active', 'false');
+        $('[aria-control="infos"]', $closest).val('');
+        $('[aria-control="input"]', $closest).val('');
+    });
+});
