365c29e31315233990f03cbfd522515e52157469
Jordy Manner <jordy.manner@milkcreation.fr>
2019-06-16 09:11:33 +0000

    tag 2.0.150 -- correctif Api\Recaptcha + Factory\Validation

---
+++ assets/partial/notice/js/scripts.js
@@ -0,0 +1,25 @@
+"use strict";
+
+jQuery(function ($) {
+  $(document).ready(function ($) {
+    $(document).on('click', '[data-control="notice"] [data-toggle="notice.dismiss"]', function (e) {
+      e.preventDefault();
+
+      $(this).closest('[data-control="notice"]').attr('aria-hide', 'true');
+    });
+
+    $(document).tifyObserver({
+          selector: '[data-control="notice"]',
+          func: function (i, target) {
+            let $el = $(target),
+                time = parseInt($el.data('timeout')) || 0;
+
+            if (time !== 0) {
+              setTimeout(function () {
+                $el.fadeOut();
+              }, time);
+            }
+          }
+    });
+  });
+});
