f4961d224a204e54fb7ae0b776671839f9d7192b
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-06-16 09:11:33 +0000

    tag 2.0.150 -- correctif Api\Recaptcha + Factory\Validation

---
+++ tags/2.0.150/src/Template/Templates/PostListTable/PostListTableServiceProvider.php
@@ -0,0 +1,121 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\PostListTable;
+
+use tiFy\Template\Templates\ListTable\ListTableServiceProvider;
+use tiFy\Template\Templates\PostListTable\Contracts\{
+    Db,
+    Params,
+    QueryBuilder,
+    PostListTable};
+
+class PostListTableServiceProvider extends ListTableServiceProvider
+{
+    /**
+     * Instance du gabarit d'affichage.
+     * @var PostListTable
+     */
+    protected $factory;
+
+    /**
+     * @inheritDoc
+     */
+    public function registerFactoryColumns(): void
+    {
+        parent::registerFactoryColumns();
+
+        $this->getContainer()->add($this->getFactoryAlias('column.post_title'),
+            function (string $name, array $attrs = []) {
+                return (new ColumnPostTitle())->setName($name)->set($attrs)->parse();
+            });
+
+        $this->getContainer()->add($this->getFactoryAlias('column.post_type'),
+            function (string $name, array $attrs = []) {
+                return (new ColumnPostType())->setTemplateFactory($this->factory)->setName($name)->set($attrs)->parse();
+            });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerFactoryDb(): void
+    {
+        $this->getContainer()->share($this->getFactoryAlias('db'), function () {
+            $ctrl = $this->factory->get('providers.db');
+            $ctrl = $ctrl instanceof Db
+                ? $ctrl
+                : $this->getContainer()->get(Db::class);
+
+            return $ctrl->setTemplateFactory($this->factory);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerFactoryLabels(): void
+    {
+        $this->getContainer()->share($this->getFactoryAlias('labels'), function () {
+            return new Labels($this->factory);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerFactoryParams(): void
+    {
+        $this->getContainer()->share($this->getFactoryAlias('params'), function () {
+            $ctrl = $this->factory->get('providers.params');
+            $ctrl = $ctrl instanceof Params
+                ? $ctrl
+                : $this->getContainer()->get(Params::class);
+
+            $attrs = $this->factory->get('params', []);
+
+            return $ctrl->setTemplateFactory($this->factory)->set(is_array($attrs) ? $attrs : [])->parse();
+        });
+    }
+
+    /**
+     * Déclaration du controleur de construction de requête.
+     *
+     * @return void
+     */
+    public function registerFactoryQueryBuilder(): void
+    {
+        $this->getContainer()->share($this->getFactoryAlias('query-builder'), function () {
+            $ctrl = $this->factory->get('providers.query-builder');
+            $ctrl = $ctrl instanceof QueryBuilder
+                ? $ctrl
+                : $this->getContainer()->get(QueryBuilder::class);
+
+            $attrs = $this->factory->param('query_args', []);
+
+            return $ctrl->setTemplateFactory($this->factory)->set(is_array($attrs) ? $attrs : []);
+        });
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerFactoryViewFilters(): void
+    {
+        parent::registerFactoryViewFilters();
+
+        $this->getContainer()->add($this->getFactoryAlias('view-filter.all'),
+            function (string $name, array $attrs = []) {
+                return (new ViewFilterAll())->setName($name)->set($attrs)->parse();
+            });
+
+        $this->getContainer()->add($this->getFactoryAlias('view-filter.publish'),
+            function (string $name, array $attrs = []) {
+                return (new ViewFilterPublish())->setName($name)->set($attrs)->parse();
+            });
+
+        $this->getContainer()->add($this->getFactoryAlias('view-filter.trash'),
+            function (string $name, array $attrs = []) {
+                return (new ViewFilterTrash())->setName($name)->set($attrs)->parse();
+            });
+    }
+}
