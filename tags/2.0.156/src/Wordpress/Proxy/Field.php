72caacae2ec8b989a5d26d1f50eec24a0aade6e6
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-06-26 17:50:40 +0000

    tag 2.0.156 -- correctif Repeater + Query saveMeta pb serialisation

---
+++ tags/2.0.156/src/Wordpress/Proxy/Field.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Proxy;
+
+use tiFy\Support\Proxy\Partial as BasePartial;
+use tiFy\Wordpress\Contracts\Field\FieldFactory;
+
+/**
+ * @method static FieldFactory|null get(string $name, array|string|null $id = null, array $attrs = [])
+ */
+class Field extends BasePartial
+{
+
+}
---
+++ tags/2.0.156/src/Wordpress/Routing/Routing.php
@@ -0,0 +1,74 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Routing;
+
+use Exception;
+use FastRoute\Dispatcher as FastRoute;
+use League\Route\Dispatcher;
+use tiFy\Contracts\Routing\{Route as RouteContract, Router as RouterManager};
+use tiFy\Http\Request;
+use tiFy\Wordpress\Contracts\Routing\Routing as RoutingContract;
+use tiFy\Wordpress\Routing\Strategy\Template as TemplateStrategy;
+
+class Routing implements RoutingContract
+{
+    /**
+     * Instance du gestionnaire de routage.
+     * @var RouterManager
+     */
+    protected $manager;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param RouterManager $manager Instance du gestionnaire de routage.
+     *
+     * @return void
+     */
+    public function __construct(RouterManager $manager)
+    {
+        $this->manager = $manager;
+
+        $this->manager->getContainer()->get('wp.wp_query');
+
+        $this->manager->getContainer()->add('router.strategy.default', function () {
+            return new TemplateStrategy();
+        });
+
+        $this->manager->getContainer()->add(
+            RouteContract::class,
+            function (string $method, string $path, callable $handler, $collection) {
+                return new Route($method, $path, $handler, $collection);
+        });
+
+        add_action('parse_request', function () {
+            try {
+                $response = $this->manager->dispatch(Request::convertToPsr());
+
+                if ($response->getStatusCode() !== 100) {
+                    $this->manager->emit($response);
+                    exit;
+                }
+            } catch (Exception $e) {
+                /**
+                 * Suppression du slash de fin dans l'url des routes déclarées.
+                 * @see https://symfony.com/doc/current/routing/redirect_trailing_slash.html
+                 * @see https://stackoverflow.com/questions/30830462/how-to-deal-with-extra-in-phpleague-route
+                 */
+                if (config('routing.remove_trailing_slash', true)) {
+                    $path = request()->getBaseUrl() . request()->getPathInfo();
+                    $method = request()->getMethod();
+
+                    if (($path != '/') && (substr($path, -1) == '/') && ($method === 'GET')) {
+                        $match = (new Dispatcher($this->manager->getData()))->dispatch($method, rtrim($path, '/'));
+
+                        if ($match[0] === FastRoute::FOUND) {
+                            wp_redirect(request()->fullUrl());
+                            exit;
+                        }
+                    }
+                }
+            }
+        }, 0);
+    }
+}
