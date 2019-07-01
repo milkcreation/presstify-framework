4c56ff925f722d0dfdf227b36e04317ce915d422
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-01 07:17:54 +0000

    tag 2.0.161 -- Correctif number-js

---
+++ src/Wordpress/Database/Concerns/MetaFieldsAwareTrait.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Concerns;
+
+use tiFy\Support\{Arr};
+
+/**
+ * @mixin \Corcel\Concerns\MetaFields
+ * @mixin \Illuminate\Database\Eloquent\Model
+ * @property $meta_value
+ */
+trait MetaFieldsAwareTrait
+{
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    public function saveMeta($key, $value = null)
+    {
+        $value = Arr::serialize($value);
+
+        if (is_array($key)) {
+            foreach ($key as $k => $v) {
+                $this->saveOneMeta($k, $v);
+            }
+            $this->load('meta');
+
+            return true;
+        }
+
+        return $this->saveOneMeta($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @return bool
+     */
+    private function saveOneMeta($key, $value)
+    {
+        $meta = $this->meta()->where('meta_key', $key)
+            ->firstOrNew(['meta_key' => $key]);
+
+        $result = $meta->fill(['meta_value' => $value])->save();
+        $this->load('meta');
+
+        return $result;
+    }
+}
