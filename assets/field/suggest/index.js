cb943940affb50b23697d928379326dd4215480a
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-04 17:04:35 +0000

    branche 2.0 -- Suggest évolution prise en compte terme de recherche

---
+++ assets/field/suggest/index.js
@@ -0,0 +1,21 @@
+// DEPS
+// jQuery-ui - Core
+import 'jquery-ui/ui/core';
+
+// jQuery-ui - Widget
+import 'jquery-ui/ui/widget';
+
+// jQuery-ui - Position
+import 'jquery-ui/ui/position';
+
+// jQuery-ui - Menu
+import 'jquery-ui/ui/widgets/menu';
+
+// jQuery-ui - Menu
+import 'jquery-ui/ui/widgets/autocomplete';
+
+// SCSS
+import './scss/styles.scss';
+
+// JS
+import './js/scripts';
