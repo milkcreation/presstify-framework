b88f289a0eb1743d1c0ce792294269525d46e924
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-07-16 16:47:27 +0000

    branch 2.0 -- correctif Sidebar

--- branches/2.0/src/Partial/Resources/views/sidebar/toggle.php
+++ branches/2.0/src/Partial/Resources/views/sidebar/toggle.php
@@ -7,28 +7,22 @@
 ?>
 
 <?php
-echo partial(
-    'tag',
-    [
-        'tag'     => 'a',
-        'attrs'   => [
-            'href'         => '#' . $this->get('attrs.id'),
-            'class'        => 'Sidebar-toggle',
-            'data-control' => 'sidebar.toggle',
-            'data-toggle'  => '#' . $this->get('attrs.id')
-        ],
-        'content' =>
-            '<svg xmlns="http://www.w3.org/2000/svg" 
-                    xmlns:xlink="http://www.w3.org/1999/xlink" 
-                    viewBox="0 0 75 75" 
-                    xml:space="preserve" 
-                    fill="' . ($this->get('theme') === 'light' ? '#2B2B2B' : '#FFF') . '"
-                >
-                    <g>
-                        <rect width="75" height="10" x="0" y="0" ry="0"/>
-                        <rect width="75" height="10" x="0" y="22" ry="0"/>
-                        <rect width="75" height="10" x="0" y="44" ry="0"/>
-                    </g>
-                </svg>'
-    ]
-);
+echo partial('tag', [
+    'tag'     => 'a',
+    'attrs'   => [
+        'href'         => '#' . $this->get('attrs.id'),
+        'class'        => 'Sidebar-toggle',
+        'data-control' => 'sidebar.toggle',
+        'data-toggle'  => '#' . $this->get('attrs.id')
+    ],
+    'content' =>
+        '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75" xml:space="preserve" 
+            fill="' . ($this->get('theme') === 'light' ? '#2B2B2B' : '#FFF') . '"
+        >
+            <g>
+                <rect width="75" height="10" x="0" y="0" ry="0"/>
+                <rect width="75" height="10" x="0" y="22" ry="0"/>
+                <rect width="75" height="10" x="0" y="44" ry="0"/>
+            </g>
+        </svg>'
+]);
