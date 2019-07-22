66e88a2f8965662132ad3a629ba8707b03f1bc4c
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-07-22 17:02:29 +0000

    branch 2.0.176 -- Template wp.admin_menu

---
+++ tags/2.0.176/src/Partial/Resources/views/holder/holder.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @var tiFy\Partial\PartialView $this
+ */
+?>
+<?php $this->before(); ?>
+
+<div <?php $this->attrs(); ?>>
+    <span class="Holder-pad"
+          style="padding-top:<?php echo ceil((100/$this->get('width'))* $this->get('height')); ?>%"
+    ></span>
+
+    <div class="Holder-content">
+        <?php
+        if ($content = $this->get('content')) :
+            echo $this->get('content');
+        else :
+            $this->insert('default', $this->all());
+        endif;
+        ?>
+    </div>
+</div>
+
+<?php $this->after();
