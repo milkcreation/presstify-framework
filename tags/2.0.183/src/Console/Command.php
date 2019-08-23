867cbca6d5760bdc36d956663fca3d3f9653290a
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-08-23 17:22:19 +0000

    tag 2.0.183 -- Correctif Command multisite

---
+++ tags/2.0.183/src/Console/Command.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Console;
+
+use Exception;
+use Symfony\Component\Console\{
+    Command\Command as BaseCommand,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+
+/**
+ * USAGE :
+ * Liste des commandes disponibles
+ * -------------------------------
+ * vendor/bin/bee list
+ *
+ * Arrêt complet des commandes CLI lancées
+ * ---------------------------------------
+ * pkill -9 php
+ */
+class Command extends BaseCommand
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure() { }
+
+    /**
+     * @inheritdoc
+     *
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) { }
+}
