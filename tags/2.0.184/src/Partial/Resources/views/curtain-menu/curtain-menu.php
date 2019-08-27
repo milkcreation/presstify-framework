bc4744369964cd67720cd255abf98db85bc02d2c
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-08-27 07:45:06 +0000

    correctif PasswordJs + Proxy\Redirect + Template\Column -> content() > value() pour permettre la surcharge

---
+++ tags/2.0.184/src/Partial/Resources/views/curtain-menu/curtain-menu.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @var tiFy\Partial\PartialView $this
+ * @var tiFy\Contracts\Partial\CurtainMenuItems $items
+ */
+?>
+<?php $this->before(); ?>
+    <div <?php $this->attrs(); ?>>
+        <nav class="CurtainMenu-nav">
+            <?php echo $this->insert('items', [
+                'items' => $items->getParentItems(null),
+                'depth' => 0,
+                'parent' => null
+            ]); ?>
+        </nav>
+    </div>
+<?php $this->after();
