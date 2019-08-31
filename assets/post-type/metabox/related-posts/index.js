58b234874981e9ba67e9487d76e8e5d3967c6052
Jordy Manner <jordy.manner@milkcreation.fr>
2019-08-31 08:42:44 +0000

    tag 2.0.190 -- Correctif Cookie multisite + Routing multisite + Validator

---
+++ assets/post-type/metabox/related-posts/index.js
@@ -0,0 +1,19 @@
+// DEPS
+// jQuery-ui - Core
+import 'jquery-ui/ui/core';
+
+// jQuery-ui - Widget
+import 'jquery-ui/ui/widget';
+
+// jQuery-ui - Sortable
+import 'jquery-ui/ui/widgets/sortable';
+
+// PresstiFy
+// Field > Suggest
+import '../../../field/suggest/index';
+
+// SCSS
+import "./scss/styles.scss";
+
+// JS
+import "./js/scripts";
---
+++ src/Partial/Partials/Dropdown/Dropdown.php
@@ -0,0 +1,91 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Partial\Partials\Dropdown;
+
+use tiFy\Contracts\Partial\{Dropdown as DropdownContract, PartialFactory as PartialFactoryContract};
+use tiFy\Contracts\Partial\DropdownItems as DropdownItemsContract;
+use tiFy\Partial\PartialFactory;
+
+class Dropdown extends PartialFactory implements DropdownContract
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return array {
+     *      @var array $attrs Attributs HTML du champ.
+     *      @var string $after Contenu placé après le champ.
+     *      @var string $before Contenu placé avant le champ.
+     *      @var array $viewer Liste des attributs de configuration du pilote d'affichage.
+     * }
+     */
+    public function defaults(): array
+    {
+        return [
+            'attrs'         => [],
+            'after'         => '',
+            'before'        => '',
+            'viewer'        => [],
+            'button'    => '',
+            'items'     => [],
+            'open'      => false,
+            'trigger'   => false
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parse(): PartialFactoryContract
+    {
+        parent::parse();
+
+        $this->set('attrs.data-control', 'dropdown');
+        $this->set('attrs.data-id', $this->getId());
+
+        $classes = [
+            'button'    => 'PartialDropdown-button',
+            'listItems' => 'PartialDropdown-items',
+            'item'      => 'PartialDropdown-item'
+        ];
+        foreach($classes as $key => &$class) {
+            $class = sprintf($this->get("classes.{$key}", '%s'), $class);
+        }
+        $this->set('classes', $classes);
+
+        $items = $this->get('items', []);
+
+        if (!$items instanceof DropdownItemsContract) {
+            $items = new DropdownItems($items);
+        }
+        $this->set('items', $items->setPartial($this));
+
+        $this->set('attrs.data-options', [
+            'classes' => $this->get('classes', []),
+            'open'    => $this->get('open'),
+            'trigger' => $this->get('trigger'),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseDefaults(): PartialFactoryContract
+    {
+        $default_class = 'Dropdown Dropdown--' . $this->getIndex();
+        if (!$this->has('attrs.class')) {
+            $this->set('attrs.class', $default_class);
+        } else {
+            $this->set('attrs.class', sprintf($this->get('attrs.class', ''), $default_class));
+        }
+
+        if (!$this->get('attrs.class')) {
+            $this->forget('attrs.class');
+        }
+
+        $this->parseViewer();
+
+        return $this;
+    }
+}
