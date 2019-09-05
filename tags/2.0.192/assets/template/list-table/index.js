45985fd9c7d6ec0c30df85172f96cc20919e3eb3
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-09-05 06:57:04 +0000

    branch 2.0 --> Partials > Mise au norme de la classe par défaut Template > ListTable > RowActions Mise en oeuvre LabelsBag > Réécriture

---
+++ tags/2.0.192/assets/template/list-table/index.js
@@ -0,0 +1,13 @@
+// DEPS
+// datatables
+import 'datatables.net-dt';
+import 'datatables.net-dt/css/jquery.dataTables.css';
+
+// jQuery-ui - widget
+import 'jquery-ui/ui/widget';
+
+// SCSS
+import './scss/styles.scss';
+
+// JS
+import './js/scripts';
