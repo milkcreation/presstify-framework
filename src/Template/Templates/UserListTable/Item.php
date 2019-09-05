9d2c79f6832feb2490d072fb290ae315af9b3b94
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-05 06:57:04 +0000

    branch 2.0 --> Partials > Mise au norme de la classe par défaut Template > ListTable > RowActions Mise en oeuvre LabelsBag > Réécriture

---
+++ src/Template/Templates/UserListTable/Item.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\UserListTable;
+
+use tiFy\Template\Templates\ListTable\Contracts\Item as BaseItemContract;
+use tiFy\Template\Templates\ListTable\Item as BaseItem;
+use tiFy\Template\Templates\UserListTable\Contracts\Item as ItemContract;
+use tiFy\Wordpress\Contracts\QueryUser as QueryUserContract;
+use tiFy\Wordpress\Query\QueryUser;
+
+/**
+ * @mixin QueryUser
+ */
+class Item extends BaseItem implements ItemContract
+{
+    /**
+     * Instance du gabarit associé.
+     * @var UserListTable
+     */
+    protected $factory;
+
+    /**
+     * Objet de délégation d'appel des méthodes de la classe.
+     * @var QueryUserContract|null
+     */
+    protected $delegate;
+
+    /**
+     * @inheritDoc
+     */
+    public function parse(): BaseItemContract
+    {
+        parent::parse();
+
+        if (is_null($this->delegate)) {
+            $this->setDelegate(QueryUser::createFromId($this->getKeyValue()));
+        }
+
+        return $this;
+    }
+}
