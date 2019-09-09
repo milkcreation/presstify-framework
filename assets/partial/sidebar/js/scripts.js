debb6b4bc93b4c891446b5d2c0146aeb1752db32
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-09 05:35:16 +0000

    tag 2.0.196 -- templates ListTable RowAction Update Row

---
+++ assets/partial/sidebar/js/scripts.js
@@ -0,0 +1,48 @@
+"use strict";
+
+jQuery(document).ready(function ($) {
+  $('body').attr('data-sidebar', true);
+
+  $(window).resize(function () {
+    $('[data-control="sidebar"]').each(function () {
+      let headerHeight = $('[data-control="sidebar.header"]', $(this)).height(),
+          footerHeight = $('[data-control="sidebar.footer"]', $(this)).height(),
+          sidebarHeight = $(this).height();
+
+      $('[data-control="sidebar.body"]').height(sidebarHeight - (headerHeight + footerHeight));
+    });
+  }).trigger('resize');
+
+  $(document)
+      .on('click', '[data-control="sidebar.toggle"]', function (e) {
+        e.preventDefault();
+
+        let $sidebar,
+            target = $(this).data('target');
+
+        if ($(target).length) {
+          $sidebar = $(target);
+        } else if ($(this).closest('[data-control="sidebar"]').length) {
+          $sidebar = $(this).closest('[data-control="sidebar"]');
+        }
+
+        if ($sidebar.length) {
+          if ($sidebar.attr('aria-closed') === 'true') {
+            $sidebar.attr('aria-closed', 'false');
+          } else {
+            $sidebar.attr('aria-closed', 'true');
+          }
+        }
+      })
+      .on('click', function (e) {
+        if (
+            !$(e.target).closest('[data-control="sidebar"][aria-closed="false"]').length &&
+            !$(e.target).closest('[data-control="sidebar.toggle"]').length
+        ) {
+          $('[data-control="sidebar"][aria-closed="false"][aria-outside_close="true"]').each(function () {
+            $(this).attr('aria-closed', 'true');
+          });
+        }
+        return true;
+      });
+});
