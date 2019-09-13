f25d869bbf17ec8396df2a2506b11ea30bab4581
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-13 17:41:32 +0000

    tag 2.0.198 -- Ajustement compatibilité bridge + nettoyage + ConsoleCommandStack + Ajustement WordpressQuery

--- src/Components/Partial/Breadcrumb/templates/breadcrumb.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @var tiFy\Partial\TemplateController $this
- */
-?>
-
-<ol <?php echo $html_attrs; ?>>
-    <?php foreach ($parts as $part) : echo $part; endforeach;?>
-</ol>
---
+++ src/Template/Templates/ListTable/Contracts/RowAction.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\ListTable\Contracts;
+
+use tiFy\Contracts\Support\ParamsBag;
+use tiFy\Contracts\Template\FactoryAwareTrait;
+
+interface RowAction extends FactoryAwareTrait, ParamsBag
+{
+    /**
+     * Résolution de sortie de la classe en tant que chaîne de caractère.
+     *
+     * @return string
+     */
+    public function __toString(): string;
+
+    /**
+     * Récupération de l'url de base du lien.
+     *
+     * @return string
+     */
+    public function getBaseUrl(): string;
+
+    /**
+     * Récupération du nom de qualification.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Controleur de requête HTTP.
+     *
+     * @return mixed
+     */
+    public function httpController();
+
+    /**
+     * Vérification de disponibilité de l'action.
+     *
+     * @return boolean
+     */
+    public function isAvailable(): bool;
+
+    /**
+     * Activation du traitement de l'action via ajax.
+     *
+     * @return boolean
+     */
+    public function isXhr(): bool;
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return static
+     */
+    public function parse(): RowAction;
+
+    /**
+     * Traitement de l'url du lien.
+     *
+     * @return static
+     */
+    public function parseUrl(): RowAction;
+
+    /**
+     * Récupération du rendu de l'affichage.
+     *
+     * @return string
+     */
+    public function render(): string;
+
+    /**
+     * Définition du nom de qualification.
+     *
+     * @param string $name Nom de qualification.
+     *
+     * @return static
+     */
+    public function setName(string $name): RowAction;
+}
