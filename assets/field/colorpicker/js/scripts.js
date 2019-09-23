7abfb3abf835c8b33b6770cfbcff9837e493f3e4
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-23 06:36:29 +0000

    tag 2.0.202 -- Réécriture assets phase#1 pb chargement ordre depuis bundle + Route > url relative par défaut

---
+++ assets/field/colorpicker/js/scripts.js
@@ -0,0 +1,19 @@
+/* global jQuery */
+"use strict";
+
+jQuery(document).ready(function ($) {
+    $(document).on('tify_field.colorpicker.init', function (event, obj) {
+        var options = $.parseJSON(
+            decodeURIComponent(
+                $(obj).data('options')
+            )
+        );
+
+        options = $.extend({change: function(color) { $(obj).val(color.toHexString()); }}, options);
+        $(obj).spectrum(options);
+    });
+
+    $('.tiFyField-colorpicker').each(function () {
+        $(document).trigger('tify_field.colorpicker.init', $(this));
+    });
+});
