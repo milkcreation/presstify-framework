fbc9833619a52bda1d16e05723e84b58ac813bb3
Jordy Manner <jordy.manner@milkcreation.fr>
2019-10-08 14:37:01 +0000

    branch 2.0 -- Correctif Suggest

--- src/tiFy.php
+++ src/tiFy.php
@@ -10,7 +10,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.214
+ * @version 2.0.215
  * @copyright Milkcreation
  */
 final class tiFy extends Container
