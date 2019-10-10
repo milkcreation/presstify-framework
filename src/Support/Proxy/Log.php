d7efdfc84261ac6b5e4f2f59c6b95cc4ffbfcd76
Jordy Manner <jordy.manner@milkcreation.fr>
2019-10-10 05:48:34 +0000

    tag 2.0.217 -- Réécriture Repeater + MediaFile

---
+++ src/Support/Proxy/Log.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Support\Proxy;
+
+use tiFy\Contracts\Log\Logger;
+
+/**
+ * @method static void emergency(string $message, array $context = [])
+ * @method static void alert(string $message, array $context = [])
+ * @method static Logger channel(?string $channel = null)
+ * @method static void critical(string $message, array $context = [])
+ * @method static void error(string $message, array $context = [])
+ * @method static void warning(string $message, array $context = [])
+ * @method static void notice(string $message, array $context = [])
+ * @method static void info(string $message, array $context = [])
+ * @method static void debug(string $message, array $context = [])
+ * @method static void log($level, string $message, array $context = [])
+ * @method static Logger|null registerChannel(string $name, array $params = [])
+ */
+class Log extends AbstractProxy
+{
+    public static function getInstanceIdentifier()
+    {
+        return 'log';
+    }
+}
