f78c1e22fb69ec4a6fbab53dadbd1540b219f7be
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-10-21 05:28:15 +0000

    tag 2.0.223 -- Form\Addon\Record phase #1 LabelBag LowerCase correctif Database MetaModel

---
+++ tags/2.0.223/src/Database/Model.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Database;
+
+use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\Query\Builder;
+use tiFy\Database\Concerns\{
+    ColumnsAwareTrait,
+    ConnectionAwareTrait
+};
+
+/**
+ * @mixin Builder
+ */
+abstract class Model extends BaseModel
+{
+    use ColumnsAwareTrait, ConnectionAwareTrait;
+}
