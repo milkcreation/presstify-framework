942a0098716d0cebb5b2b96326dadffc1973caee
gaetanthery <gaetanthery@59481347-5d8a-4110-b049-6281aa622e7a>
2019-10-21 08:04:41 +0000

    presstify-framework -- tag 2.0.191 -- Correction url pagehook

--- tags/2.0.191/src/tiFy.php
+++ tags/2.0.191/src/tiFy.php
@@ -12,7 +12,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.190
+ * @version 2.0.191
  * @copyright Milkcreation
  */
 final class tiFy extends Container
