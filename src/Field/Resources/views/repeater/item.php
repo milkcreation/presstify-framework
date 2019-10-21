aee445ea555f77db11fd16761827653d018dcaa4
Jordy Manner <jordy.manner@milkcreation.fr>
2019-10-21 16:46:23 +0000

    tag 2.0.224 -- Wordpress\Query réécriture

---
+++ src/Field/Resources/views/repeater/item.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @var tiFy\Field\FieldView $this
+ */
+?>
+<?php echo field('text', [
+    'name'  => "{$this->getName()}[{$this->get('index')}]",
+    'value' => $this->get('value'),
+]);
