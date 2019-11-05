9349b91d7dc5089a34d5773b0818265f9e090925
Jordy Manner <jordy.manner@milkcreation.fr>
2019-11-05 07:37:24 +0000

    tag 2.0.226 -- Proxy Schema phase #1 + correctif Template\ListTable viewer

---
+++ src/Form/AddonMetaboxDriver.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Form;
+
+use LogicException;
+use tiFy\Contracts\{Form\FormFactory, Form\AddonFactory};
+use tiFy\Metabox\{MetaboxDriver, MetaboxView};
+
+abstract class AddonMetaboxDriver extends MetaboxDriver
+{
+    /**
+     * Instance de l'addon de formulaire associé.
+     * @var AddonFactory|null
+     */
+    protected $addon;
+
+    /**
+     * Instance du formulaire associé.
+     * @var FormFactory|null
+     */
+    protected $form;
+
+    /**
+     * Récupération de l'instance de l'addon associé.
+     *
+     * @return AddonFactory
+     */
+    public function addon(): AddonFactory
+    {
+        if ($this->addon instanceof AddonFactory) {
+            return $this->addon;
+        }
+
+        throw new LogicException(
+            __('Aucune instance d\'addon de formulaire n\'est associé à la boîte de saisie.', 'tify')
+        );
+    }
+
+    /**
+     * Récupération de l'instance du formulaire associé.
+     *
+     * @return FormFactory
+     */
+    public function form(): FormFactory
+    {
+        return $this->addon()->form();
+    }
+
+    /**
+     * Définition de l'addon associé.
+     *
+     * @param AddonFactory $addon
+     *
+     * @return $this
+     */
+    public function setAddon(AddonFactory $addon): self
+    {
+        $this->addon = $addon;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function viewer(?string $view = null, array $data = [])
+    {
+        if (!$this->viewer) {
+            $defaultDir = "{$this->form()->viewer()->getDirectory()}/addon/{$this->addon()->name()}/metabox";
+            $fallbackDir = $this->get('viewer.override_dir') ?: $defaultDir;
+
+            $this->viewer = view()
+                ->setDirectory($defaultDir)
+                ->setOverrideDir($fallbackDir)
+                ->setController(MetaboxView::class)
+                ->setParam('metabox', $this);
+        }
+
+        if (func_num_args() === 0) {
+            return $this->viewer;
+        }
+
+        return $this->viewer->make("_override::{$view}", $data);
+    }
+}
---
+++ src/Partial/Partials/CookieNotice/CookieNotice.php
@@ -0,0 +1,146 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Partial\Partials\CookieNotice;
+
+use Closure;
+use Exception;
+use tiFy\Contracts\Partial\{CookieNotice as CookieNoticeContract, PartialFactory as PartialFactoryContract};
+use tiFy\Contracts\Routing\Route;
+use tiFy\Partial\PartialFactory;
+use tiFy\Support\Proxy\{Cookie, Request, Router};
+
+class CookieNotice extends PartialFactory implements CookieNoticeContract
+{
+    /**
+     * Url de traitement de requête XHR.
+     * @var Route|string
+     */
+    protected $url = '';
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void
+    {
+        $this->setUrl();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return array {
+     * @var array $attrs Attributs HTML du champ.
+     * @var string $after Contenu placé après le champ.
+     * @var string $before Contenu placé avant le champ.
+     * @var array $viewer Liste des attributs de configuration du pilote d'affichage.
+     * @var string|callable $content Texte du message de notification. défaut 'Lorem ipsum dolor site amet'.
+     * @var array $cookie Liste des paramètre de cookie. @see tiFy\Cookie\Cookie
+     * @var bool $dismiss Affichage du bouton de masquage de la notification.
+     * @var string $type Type de notification info|warning|success|error. défaut info.
+     * @var array $trigger Attribut de configuration du lien de validation et de création du cookie.
+     * }
+     */
+    public function defaults(): array
+    {
+        return [
+            'attrs'   => [],
+            'after'   => '',
+            'before'  => '',
+            'viewer'  => [],
+            'content' => '<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>',
+            'cookie'  => [],
+            'dismiss' => false,
+            'type'    => 'info',
+            'trigger' => [],
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getUrl(...$params): string
+    {
+        return $this->url instanceof Route ? $this->url->getUrl($params) : $this->url;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parse(): PartialFactoryContract
+    {
+        parent::parse();
+
+        $content = $this->get('content', '');
+        $this->set('content', $content instanceof Closure ? call_user_func($content) : $content);
+
+        $cookie = Cookie::instance($this->getId(), array_merge(['value' => '1'], $this->get('cookie', [])));
+
+        if ($cookie->get()) {
+            $this->set('attrs.aria-hide', 'true');
+        }
+
+        $this->set('attrs.data-options', [
+            'ajax'    => [
+                'url' => $this->getUrl(),
+                'data' => [
+                    '_id'     => $this->getId(),
+                    '_cookie' => $this->get('cookie', []),
+                ],
+                'method' => 'POST'
+            ]
+        ]);
+
+        if ($trigger = $this->get('trigger', [])) {
+            $this->set('content', $this->get('content', '') . $this->trigger(is_array($trigger) ?: []));
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setUrl(?string $url = null): PartialFactoryContract
+    {
+        $this->url = is_null($url) ? Router::xhr(md5($this->getAlias()), [$this, 'xhrResponse']) : $url;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function trigger($args = []): string
+    {
+        $args = array_merge([
+            'tag'     => 'a',
+            'attrs'   => [],
+            'content' => __('Fermer', 'tify'),
+        ], $args);
+
+        if (($args['tag'] === 'a') && !isset($args['attrs']['href'])) {
+            $args['attrs']['href'] = '#';
+        }
+
+        $args['attrs']['data-toggle'] = 'notice.trigger';
+
+        return (string)$this->manager->get('tag', $args);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function xhrResponse(...$args): array
+    {
+        $id = Request::input('_id');
+
+        try {
+            $cookie = Cookie::instance($id, Request::input('_cookie', []));
+
+            $cookie->set('1');
+            return ['success' => true];
+        } catch (Exception $e) {
+            return ['success' => false];
+        }
+    }
+}
