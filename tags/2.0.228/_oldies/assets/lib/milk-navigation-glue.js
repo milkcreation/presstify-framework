d6cf4b59fd2671d8222eba815e1039e75d21954a
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-11-29 14:32:26 +0000

    tag 2.0.228 -- Proxy\Mailer + Correctif MessagesBag

---
+++ tags/2.0.228/_oldies/assets/lib/milk-navigation-glue.js
@@ -0,0 +1,23 @@
+!( function( $, doc, win, undefined ){
+	var $window = $(window), $moveable = $( '#primary-navigation' ), $glue = $( '#jumbotron' ); 
+	
+	var fixedTop = false, fixedBottom = false, moveable = false;
+	
+	$(window).scroll( function (e) {
+        if ( $(this).scrollTop() >  $glue.position().top ){      	
+			if( fixedTop )return false;
+			fixedTop = true, fixedBottom = false, moveable = false;
+			$moveable.css({ top:0, bottom: 'auto', 'position' : 'fixed' });
+			// console.log( 'FixedTop' );
+		} else if ( $(this).scrollTop() <  $glue.position().top - $(this).height() + $moveable.height() ) {
+			if( fixedBottom )return false;
+			fixedTop = false, fixedBottom = true, moveable = false;
+			$moveable.css({ top:'auto', bottom:0, position:'fixed' });
+			// console.log( 'FixedBottom' );
+		} else {
+			fixedTop = false, fixedBottom = false, moveable = true;
+			$moveable.css({ top:'auto', bottom:'auto', position:'absolute' });
+			// console.log( 'move' );
+		}
+    });
+})( jQuery, document, window, undefined );
