9d24a2981fb4664c60b40263c0dddab831376d5b
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-17 06:04:08 +0000

    tag 2.0.235 -- PartialFactory correctif mineur

---
+++ src/Taxonomy/TaxonomyServiceProvider.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Taxonomy;
+
+use tiFy\Container\ServiceProvider;
+
+class TaxonomyServiceProvider extends ServiceProvider
+{
+    /**
+     * Liste des noms de qualification des services fournis.
+     * @internal requis. Tous les noms de qualification de services à traiter doivent être renseignés.
+     * @var string[]
+     */
+    protected $provides = [
+        'taxonomy',
+        'taxonomy.term-meta'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function register(): void
+    {
+        $this->registerManager();
+        $this->registerTermMeta();
+    }
+
+    /**
+     * Déclaration du controleur principal.
+     *
+     * @return void
+     */
+    public function registerManager(): void
+    {
+        $this->getContainer()->share('taxonomy', function () {
+            return new TaxonomyManager($this->getContainer());
+        });
+    }
+
+    /**
+     * Déclaration du controleur de gestion des metadonnées de terme d'une taxonomie.
+     *
+     * @return void
+     */
+    public function registerTermMeta(): void
+    {
+        $this->getContainer()->share('taxonomy.term-meta', function () {
+            return new TaxonomyTermMeta();
+        });
+    }
+}
---
+++ src/Wordpress/Contracts/PageHook.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Contracts;
+
+use Illuminate\Support\Collection;
+
+interface PageHook
+{
+    /**
+     * Récupération de la liste des instances des pages d'accroche déclarées.
+     *
+     * @return PageHookItem[]|array
+     */
+    public function all(): array;
+
+    /**
+     * Récupération de la collection des instances des pages d'accroche déclarées.
+     *
+     * @return Collection
+     */
+    public function collect(): Collection;
+
+    /**
+     * Récupération de la classe de rappel d'une page d'accroche déclarée.
+     *
+     * @param string $name Nom de qualification.
+     *
+     * @return PageHookItem|null
+     */
+    public function get($name): ?PageHookItem;
+
+    /**
+     * Vérification d'existance d'un contenu d'accroche sur la page d'affichage courante.
+     *
+     * @return boolean
+     */
+    public function has(): bool;
+
+    /**
+     * Déclaration de page d'accroche.
+     *
+     * @param string|array $name Nom de qualification ou liste des pages à déclarer.
+     * @param array $attrs Liste des attributs de configuration .
+     *
+     * @return static
+     */
+    public function set($name, $attrs = []): PageHook;
+}
