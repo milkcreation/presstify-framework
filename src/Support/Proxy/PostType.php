f772bdb15c3cda23b8d1ebbd5e29d3e74f85816a
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-20 16:52:53 +0000

    tag 2.0.240 -- correctif tinyMCE + Column\PostThumbnail

---
+++ src/Support/Proxy/PostType.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Support\Proxy;
+
+use tiFy\Contracts\PostType\{PostType as PostTypeContract, PostTypeFactory, PostTypePostMeta, PostTypeStatus};
+
+/**
+ * @method static PostTypeFactory|null get(string $name)
+ * @method static PostTypePostMeta meta()
+ * @method static PostTypeFactory register(string $name, array $args = [])
+ * @method static PostTypeStatus status(string $name, array $args = [])
+ */
+class PostType extends AbstractProxy
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return PostTypeContract
+     */
+    public static function getInstance()
+    {
+        return parent::getInstance();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getInstanceIdentifier()
+    {
+        return 'post-type';
+    }
+}
