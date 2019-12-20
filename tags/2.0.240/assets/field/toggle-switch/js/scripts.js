9ffb10225a6565048ca5a530b32c459e3c6f272c
mastermilk <mastermilk@59481347-5d8a-4110-b049-6281aa622e7a>
2019-12-20 16:52:53 +0000

    tag 2.0.240 -- correctif tinyMCE + Column\PostThumbnail

---
+++ tags/2.0.240/assets/field/toggle-switch/js/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import jQuery from 'jquery';
+
+jQuery(function ($) {
+  $(document).on('change', '.tiFyField-toggleSwitchRadio', function () {
+    $(this)
+        .closest('.tiFyField-toggleSwitch')
+        .trigger('tify_field.toggleSwitch.change', $(this).val());
+  });
+});
---
+++ tags/2.0.240/src/Wordpress/Partial/PartialFactory.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Partial;
+
+use tiFy\Contracts\Partial\PartialFactory as BasePartialFactoryContract;
+use tiFy\Partial\PartialFactory as BasePartialFactory;
+
+abstract class PartialFactory extends BasePartialFactory
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return $this
+     */
+    public function parse(): BasePartialFactoryContract
+    {
+        if (file_exists(__DIR__ . '/Resources/views/' . $this->getAlias())) {
+            $this->set('viewer.directory', __DIR__ . '/Resources/views/' . $this->getAlias());
+        }
+
+        parent::parse();
+
+        $this->parseDefaults();
+
+        return $this;
+    }
+}
