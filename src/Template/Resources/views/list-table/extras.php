a731ced6d05c63e9c5b36cbc273ead88f1718940
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-24 14:55:04 +0000

    tag 2.0.241 -- animate + hover > anim && Partial\Accordion passe corrective + divers

---
+++ src/Template/Resources/views/list-table/extras.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Interface de navigation complémentaire.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\ListTable\Viewer $this
+ * @var string $which top|bottom.
+ */
+echo $this->extras();
---
+++ src/Wordpress/Partial/Partials/Accordion/AccordionWpTermWalker.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Partial\Partials\Accordion;
+
+use tiFy\Partial\Partials\Accordion\AccordionWalkerItems;
+use WP_Term;
+use WP_Term_Query;
+
+class AccordionWpTermWalker extends AccordionWalkerItems
+{
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param AccordionWpTerm[]|WP_Term[] $terms Liste des éléments.
+     * @param array|null $opened Liste des éléments ouverts.
+     *
+     * @return void
+     */
+    public function __construct($terms, $opened = null)
+    {
+        $items = [];
+        array_walk($terms, function ($term, $key) use (&$items) {
+            if ($term instanceof WP_Term) {
+                $key = $term->term_id;
+
+                $items[$key] = new AccordionWpTerm($key, $term);
+            } elseif ($term instanceof AccordionWpTerm) {
+                $items[$key] = $term;
+            }
+        });
+
+        parent::__construct($items, $opened);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param WP_Term_Query|array $args Requête de récupération de termes ou liste des arguments de requête de
+     *                                  récupération.
+     *                                  @see https://developer.wordpress.org/reference/classes/wp_term_query/
+     */
+    public static function query($args, $opened = null): self
+    {
+        if (!$args instanceof WP_Term_Query) {
+            $args = new WP_Term_Query($args);
+        }
+
+        return new static($args->terms ? : [], $opened);
+    }
+}
