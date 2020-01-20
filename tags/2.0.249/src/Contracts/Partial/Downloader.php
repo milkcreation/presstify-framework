205af5b1db553599f42c9c6016e8ca6e026fa26d
5upp0r7 <5upp0r7@59481347-5d8a-4110-b049-6281aa622e7a>
2020-01-20 07:52:47 +0000

    tag 2.0.249 -- Ajout de dépendance + Correctifs : Sidebar + PageHookItem + BreadcrumbCollection

---
+++ tags/2.0.249/src/Contracts/Partial/Downloader.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Contracts\Partial;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response as Response;
+
+interface Downloader extends PartialDriver
+{
+    /**
+     * Récupération de l'url de requête HTTP.
+     *
+     * @param array ...$params Liste des paramètres optionnels de formatage de l'url.
+     *
+     * @return string
+     */
+    public function getUrl(...$params): string;
+
+    /**
+     * Définition de l'url de requête HTTP.
+     *
+     * @param string|null $url
+     *
+     * @return static
+     */
+    public function setUrl(?string $url = null): Downloader;
+
+    /**
+     * Récupération du chemin absolu du fichier à téléchargé basé sur une liste d'arguments.
+     *
+     * @param array ...$args Liste des arguments de récupération du chemin absolu.
+     *
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function getFilename(...$args): string;
+
+    /**
+     * Controleur de traitement de la requête HTTP.
+     *
+     * @param string $path
+     *
+     * @return Response
+     */
+    public function getResponse(string $path): Response;
+}
