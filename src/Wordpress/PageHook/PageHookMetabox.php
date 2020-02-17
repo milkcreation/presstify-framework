5844f673b74a94bdb70c46b8eaf0d3ed2ff95487
Jordy Manner <jordy.manner@milkcreation.fr>
2020-02-17 17:55:09 +0000

    2.0.258 -- Réécriture image Lightbox + évol WP && number jS method spinner

---
+++ src/Wordpress/PageHook/PageHookMetabox.php
@@ -0,0 +1,76 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\PageHook;
+
+use tiFy\Contracts\Metabox\MetaboxDriver as MetaboxDriverContract;
+use tiFy\Metabox\MetaboxDriver;
+use tiFy\Wordpress\Contracts\PageHook;
+
+class PageHookMetabox extends MetaboxDriver
+{
+    /**
+     * Instance du gestionnaire de page d'accroche.
+     * @var PageHook|null
+     */
+    protected $pageHook;
+
+    /**
+     * @inheritDoc
+     */
+    public function defaults(): array
+    {
+        return array_merge(parent::defaults(), [
+            'title' => __('Pages d\'accroche', 'tify'),
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parse(): MetaboxDriverContract
+    {
+        parent::parse();
+
+        $this->set([
+            'items' => $this->pageHook()->collect()->where('admin', true)->all(),
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * Récupération du gestionnaire de pages d'accroche.
+     *
+     * @return PageHook
+     */
+    public function pageHook(): ?PageHook
+    {
+        return $this->pageHook;
+    }
+
+    /**
+     * Définition du gestionnaire de pages d'accroche.
+     *
+     * @param PageHook $pageHook
+     *
+     * @return $this
+     */
+    public function setPageHook(PageHook $pageHook): self
+    {
+        $this->pageHook = $pageHook;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function viewer(?string $view = null, array $data = [])
+    {
+        if (!$this->viewer && !$this->get('viewer.directory')) {
+            $this->set('viewer.directory', class_info($this->pageHook)->getDirname() . '/Resources/views/metabox');
+        }
+
+        return func_num_args() === 0 ? parent::viewer() : parent::viewer($view, $data);
+    }
+}
