5e25162fef998212fa1f1ee491baef46b654fb3e
Jordy Manner <jordy.manner@milkcreation.fr>
2020-02-24 08:05:03 +0000

    2.0 -- pb compatibilité emogrifer 7.4

--- src/tiFy.php
+++ src/tiFy.php
@@ -10,7 +10,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.260
+ * @version 2.0.261
  * @copyright Milkcreation
  */
 final class tiFy extends Container
