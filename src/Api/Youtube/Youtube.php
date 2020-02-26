5d39a6d4abdcdd1a7c4e342319192cfd69f1f480
Jordy Manner <jordy.manner@milkcreation.fr>
2020-02-26 10:14:22 +0000

    2.0.262 -- Correctif File-manager + StaticCacheManager + Passe Templates

--- src/Api/Youtube/Youtube.php
+++ src/Api/Youtube/Youtube.php
@@ -5,50 +5,58 @@
  * @see https://github.com/oscarotero/Embed
  * @see https://oscarotero.com/embed3/demo/index.php
  */
+
 namespace tiFy\Api\Youtube;
 
+use Exception;
 use Illuminate\Support\Arr;
 use Embed\Embed;
-use tiFy\Apps\AppTrait;
+use Madcoda\Youtube\Youtube as MadcodaYoutube;
+use tiFy\Support\Proxy\{Partial, View};
+use tiFy\Validation\Validator as v;
+use WP_Error;
 
-class Youtube extends \Madcoda\Youtube\Youtube
+class Youtube extends MadcodaYoutube
 {
-    use AppTrait;
-
     /**
-     * Instance de la classe
-     * @var tiFy\Api\Youtube\Youtube
+     * Instance de la classe.
+     * @var static|null
      */
-    static $Inst        = null;
+    protected static $instance;
 
     /**
-     * CONSTRUCTEUR
+     * CONSTRUCTEUR.
      *
-     * @param array $param
-     * @param $sslPath
+     * @param array $params Liste des paramètres.
+     * @param string $sslPath
      *
      * @return void
+     *
+     * @throws Exception
      */
-    public function __construct($params = [], $sslPath = null)
+    protected function __construct($params = [], $sslPath = null)
     {
         parent::__construct($params, $sslPath);
     }
-    
+
     /**
-     * CONTROLEURS
-     */
-    /**
-     * Initialisation
+     * Instanciation.
      *
      * @param array $attrs
      *
-     * @return void
+     * @return static
+     *
+     * @throws Exception
      */
-    public static function create($attrs = [])
+    public static function create(array $attrs = [])
     {
-        return self::$Inst = new static($attrs, is_ssl());
+        if (!self::$instance) {
+            self::$instance = new static($attrs, is_ssl());
+        }
+
+        return self::$instance;
     }
-    
+
     /**
      * Vérification de correspondance d'url.
      *
@@ -58,93 +66,169 @@ class Youtube extends \Madcoda\Youtube\Youtube
      */
     public static function isUrl($url)
     {
-        return preg_match('#^https?://(?:www\.)?(?:youtube\.com/watch|youtu\.be/)#', $url);
-    }
-
-    /**
-     * Récupération de miniature.
-     *
-     * @param string $url Url de la vidéo.
-     * @param string $formats Format de l'image, par ordre de préférence.
-     *
-     * @return array
-     */
-    public static function getThumbnailSrc($url, $formats = ['maxres','standard', 'height', 'medium', 'default'])
-    {
-        if (!$inst = self::$Inst) :
-            return;
-        endif;
-
-        // Vérification de l'url de la vidéo
-        if (! self::isUrl($url)) :
-            return new \WP_Error('tFyComponentsApiYtInvalidSrc', __('Url YouTube invalide', 'tify'));
-        endif;
-
-        // Récupération de l'ID de la vidéo
-        if (! $ytid = self::parseVIdFromURL($url)) :
-            return new \WP_Error('tFyComponentsApiYtParseVIdFailed', __('Récupération de l\ID de la vidéo depuis l\'url en échec', 'tify'));
-        endif;
-
-        // Récupération de infos de la vidéo
-        if (! $infos = $inst->getVideoInfo($ytid)) :
-            return new \WP_Error('tFyComponentsApiYtGetVideoInfos', __('Impossible de récupérer les informations de la vidéo', 'tify'));
-        endif;
-
-        // Récupération de la liste des miniatures
-        if (empty($infos->snippet->thumbnails)) :
-            return new \WP_Error('tFyComponentsApiYtAnyThumbnailAvailable', __('Aucune miniature disponible', 'tify'));
-        endif;
-
-        foreach ($formats as $format) :
-            if(empty($infos->snippet->thumbnails->{$format})) :
-                continue;
-            endif;
-            $attrs = $infos->snippet->thumbnails->{$format};
-            if(empty($attrs->url) || empty($attrs->width) || empty($attrs->height) ) :
-                continue;
-            endif;
-
-            $src = array_values((array) $attrs);
-            $src[] = $format;
-
-            return $src;
-        endforeach;
+        return preg_match('#^https?:\/\/(?:www\.)?(?:youtube\.com/watch|youtu\.be/)#', $url);
     }
 
     /**
      * Récupération du code d'intégration d'une vidéo.
      *
-     * @param string $url Url de la video.
+     * @param string $video Identifiant ou url de la vidéo.
      * @param array $params {
      *      Liste des paramètres.
      *      @see https://developers.google.com/youtube/player_parameters?hl=fr#Parameters
      * }
      *
-     * @return string|void
+     * @return string
      */
-    public function getVideoEmbed($url, $params = [])
+    public function getEmbed($video, $params = [])
     {
-        try{
-            $id = self::parseVIdFromURL($url);
-        } catch (\Exception $e) {
-            return;
+        if(v::url()->validate($video)) {
+            try {
+                $video_id = self::parseVIdFromURL($video);
+                $video_url = $video;
+            } catch (Exception $e) {
+                return '';
+            }
+        } else {
+            try {
+                $video_id = $video;
+                $video_url = $this->getUrlFromId($video);
+            } catch (Exception $e) {
+                return '';
+            }
         }
 
-        try{
-            $info = Embed::create($url);
+        try {
+            $info = Embed::create($video_url);
 
-            if (Arr::get($params, 'loop')) :
-                Arr::set($params, 'playlist', $id);
-            endif;
+            if (Arr::get($params, 'loop')) {
+                Arr::set($params, 'playlist', $video_id);
+            }
 
             $height = $info->getHeight();
             $ratio = $info->getAspectRatio();
-            $src = esc_url("//www.youtube.com/embed/{$id}". ($params ? '?' . http_build_query($params) : ''));
+            $src = esc_url("//www.youtube.com/embed/{$video_id}" . ($params ? '?' . http_build_query($params) : ''));
             $width = $info->getWidth();
 
-            return $this->appTemplateRender('iframe', compact('height', 'ratio', 'src', 'width'));
-        } catch (\Exception $e) {
-            return;
+            return View::getPlatesEngine(['directory' => __DIR__ . '/views'])->render(
+                'iframe',
+                compact('height', 'ratio', 'src', 'width', 'params')
+            );
+        } catch (Exception $e) {
+            return '';
+        }
+    }
+
+    /**
+     * Récupération des données de miniature.
+     *
+     * @param string $video Identifiant ou url de la vidéo.
+     * @param array $formats Format de l'image, par ordre de préférence. maxres|standard|height|medium|default.
+     *
+     * @return array|WP_Error
+     *
+     * @throws Exception
+     */
+    public function getThumbnailSrc($video, $formats = [])
+    {
+        if(v::url()->validate($video)) {
+            if (!self::isUrl($video)) {
+                return new WP_Error(
+                    'tFyComponentsApiYtInvalidSrc',
+                    __('Url YouTube invalide', 'tify')
+                );
+            } elseif (!$video = self::parseVIdFromURL($video)) {
+                return new WP_Error(
+                    'tFyComponentsApiYtParseVIdFailed',
+                    __('Récupération de l\ID de la vidéo depuis l\'url en échec', 'tify')
+                );
+            }
+        }
+
+        if (!$infos = $this->getVideoInfo($video)) {
+            return new WP_Error(
+                'tFyComponentsApiYtGetVideoInfos',
+                __('Impossible de récupérer les informations de la vidéo', 'tify')
+            );
+        }
+
+        if (empty($infos->snippet->thumbnails)) {
+            return new WP_Error(
+                'tFyComponentsApiYtAnyThumbnailAvailable',
+                __('Aucune miniature disponible', 'tify')
+            );
+        }
+
+        if (empty($formats)) {
+            $formats = array_keys(get_object_vars($infos->snippet->thumbnails));
+        }
+
+        foreach ($formats as $format) {
+            if (empty($infos->snippet->thumbnails->{$format})) {
+                continue;
+            }
+
+            $attrs = get_object_vars($infos->snippet->thumbnails->{$format});
+            if (empty($attrs['url']) || empty($attrs['width']) || empty($attrs['height'])) {
+                continue;
+            }
+
+            $attrs['src'] = $attrs['url'];
+            unset($attrs['url']);
+
+            $attrs['title'] = $infos->snippet->title;
+
+            $src[$format] = $attrs;
+        }
+
+        return !empty($src) ? $src : [];
+    }
+
+    /**
+     * Récupération des données de miniature.
+     *
+     * @param string $video Identifiant ou url de la vidéo.
+     * @param string $size Taille de l'image.
+     *
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function getThumbnailImg($video, $size = 'default')
+    {
+        $attrs = $this->getThumbnailSrc($video, [$size]);
+
+        if ($attrs && !is_wp_error($attrs)) {
+            $attrs = reset($attrs);
+
+            return (string)Partial::get('tag', [
+                'tag'   => 'img',
+                'attrs' => $attrs
+            ]);
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * Récupération de l'url à partir de l'Id de la video.
+     *
+     * @param string $id Identifiant de la video.
+     *
+     * @return string
+     *
+     * @throws Exception
+     */
+    public function getUrlFromId($id)
+    {
+        $url = "https://www.youtube.com/watch?v={$id}";
+
+        try {
+            self::parseVIdFromURL($url);
+
+            return $url;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
         }
     }
 }
