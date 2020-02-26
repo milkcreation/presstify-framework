a639078594837c545cf099ae05d19d63f6bcea29
5upp0r7 <5upp0r7@59481347-5d8a-4110-b049-6281aa622e7a>
2020-02-26 10:14:22 +0000

    2.0.262 -- Correctif File-manager + StaticCacheManager + Passe Templates

---
+++ tags/2.0.262/src/Wordpress/Partial/Driver/Holder/Holder.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Partial\Driver\Holder;
+
+use tiFy\Partial\Driver\Holder\Holder as BaseHolder;
+use tiFy\Wordpress\Contracts\Partial\PartialDriver as PartialDriverContract;
+
+class Holder extends BaseHolder implements PartialDriverContract
+{
+
+}
