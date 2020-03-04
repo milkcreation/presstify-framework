11af93804f3749e89c90f43da568761b7cdc7a8a
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-03-04 15:18:58 +0000

    2.0.268 -- Correctif Template list-table Colum-toggle

---
+++ tags/2.0.268/src/Kernel/Application.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Kernel;
+
+use BadMethodCallException;
+use Exception;
+use Interop\Container\ContainerInterface;
+use tiFy\Container\Container;
+use XStatic\ProxyManager;
+
+class Application extends Container
+{
+    /**
+     * Instance du conteneur d'injection de dépendances.
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param ContainerInterface $container
+     *
+     * @return void
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        $this->delegate($container);
+
+        parent::__construct();
+
+        $this->registerProxy();
+
+        $this->boot();
+    }
+
+    /**
+     * Délégation d'appel des méthodes du conteneur d'injection.
+     *
+     * @param string $name Nom de la méthode à appeler.
+     * @param array $arguments Liste des variables passées en argument.
+     *
+     * @return mixed
+     *
+     * @throws BadMethodCallException
+     */
+    public function __call(string $name, array $arguments)
+    {
+        try {
+            return $this->container->$name(...$arguments);
+        } catch (Exception $e) {
+            throw new BadMethodCallException(sprintf(__('La méthode %s n\'est pas disponible.', 'tify'), $name));
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void {}
+
+    /**
+     * @inheritDoc
+     */
+    public function getServiceProviders(): array
+    {
+        return config('app.providers', []);
+    }
+
+    /**
+     * Détermine si l'application est lancée dans une console.
+     *
+     * @return boolean
+     */
+    public function runningInConsole(): bool
+    {
+        global $argv;
+
+        if (isset($_ENV['APP_RUNNING_IN_CONSOLE'])) {
+            return $_ENV['APP_RUNNING_IN_CONSOLE'] === 'true';
+        } elseif(isset($argv[0]) && preg_match('/vendor\/bin\/bee$/', $argv[0])) {
+            return true;
+        }
+        return php_sapi_name() === 'cli' || php_sapi_name() === 'phpdbg';
+    }
+
+    /**
+     *
+     */
+    public function registerProxy()
+    {
+        $manager = new ProxyManager($this);
+        foreach(config('app.proxy', []) as $alias => $proxy) {
+            $manager->addProxy($alias, $proxy);
+        }
+        $manager->enable(ProxyManager::ROOT_NAMESPACE_ANY);
+    }
+
+    /**
+     * Compatibilité corcel.
+     *
+     * @return string
+     */
+    public function version(): string
+    {
+        return 'tiFy';
+    }
+}
---
+++ tags/2.0.268/src/Template/Templates/ListTable/DbBuilder.php
@@ -0,0 +1,86 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Template\Templates\ListTable;
+
+use tiFy\Contracts\Template\FactoryBuilder as FactoryBuilderContract;
+use tiFy\Template\Factory\DbBuilder as BaseDbBuilder;
+use tiFy\Template\Templates\ListTable\Contracts\{DbBuilder as DbBuilderContract, Item};
+
+class DbBuilder extends BaseDbBuilder implements DbBuilderContract
+{
+    /**
+     * Instance du gabarit associé.
+     * @var Factory
+     */
+    protected $factory;
+
+    /**
+     * @inheritDoc
+     */
+    public function getItem(string $key): ?Item
+    {
+        $this->parse();
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fetchItems(): DbBuilderContract
+    {
+        $this->parse();
+
+        $this->querySearch();
+        $this->queryWhere();
+        $this->queryOrder();
+
+        $total = $this->query()->count();
+
+        if ($total < $this->getPerPage()) {
+            $this->setPage(1);
+        }
+
+        $this->queryLimit();
+        $items = $this->query()->get();
+
+        $this->factory->items()->set($items);
+
+        if ($count = $items->count()) {
+            $this->factory->pagination()
+                ->setCount($count)
+                ->setPage($this->getPage())
+                ->setPerPage($this->getPerPage())
+                ->setLastPage((int)ceil($total / $this->getPerPage()))
+                ->setTotal($total)
+                ->parse();
+        }
+
+        $this->resetQuery();
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return DbBuilderContract
+     */
+    public function parse(): FactoryBuilderContract
+    {
+        parent::parse();
+
+        if ($this->factory->ajax() && $this->pull('draw', 0)) {
+            $this
+                ->setSearch((string)$this->get('search.value', $this->getSearch()))
+                ->setPerPage((int)$this->pull('length', $this->getPerPage()))
+                ->setPage((int)ceil(($this->pull('start') / $this->getPerPage()) + 1));
+
+            $this->pull('columns');
+            $this->pull('search');
+            $this->pull('action');
+        }
+
+        return $this;
+    }
+}
