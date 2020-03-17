acda23f85e60670082d6b30bc762fbf59d2b6c4c
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-03-17 08:57:37 +0000

    2.0.275 -- Ajustement assets modal

---
+++ tags/2.0.275/src/Contracts/Form/FormFactory.php
@@ -0,0 +1,178 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Contracts\Form;
+
+use tiFy\Contracts\Support\{LabelsBag, ParamsBag};
+
+interface FormFactory extends FactoryResolver, ParamsBag
+{
+    /**
+     * Résolution de sortie de l'affichage du formulaire.
+     *
+     * @return string
+     */
+    public function __toString(): string;
+
+    /**
+     * Initialisation du contrôleur.
+     *
+     * @return void
+     */
+    public function boot(): void;
+
+    /**
+     * Récupération de la chaîne de sécurisation du formulaire (CSRF).
+     *
+     * @return string
+     */
+    public function csrf(): string;
+
+    /**
+     * Déclaration d'un message d'erreur.
+     *
+     * @param string $message Intitulé du message.
+     * @param array $datas Données associées à l'erreur
+     *
+     * @return string Identifiant de qualification du message d'erreur
+     */
+    public function error(string $message, array $datas = []): string;
+
+    /**
+     * Récupération de valeur(s) de champ(s) basée(s) sur leurs variables d'identifiant de qualification.
+     *
+     * @param mixed $tags Variables de qualification de champs.
+     * string ex. "%%{{slug#1}}%% %%{{slug#2}}%%"
+     * array ex ["%%{{slug#1}}%%", "%%{{slug#2}}%%"]
+     * @param boolean $raw Activation de la valeur de retour au format brut.
+     *
+     * @return string
+     */
+    public function fieldTagValue($tags, $raw = true);
+
+    /**
+     * Récupération de l'action du formulaire (url).
+     *
+     * @return string
+     */
+    public function getAction(): string;
+
+    /**
+     * Récupération de la méthode de soumission du formulaire.
+     *
+     * @return string
+     */
+    public function getMethod(): string;
+
+    /**
+     * Récupération de l'intitulé de qualification du formulaire.
+     *
+     * @return string
+     */
+    public function getTitle(): string;
+
+    /**
+     * Vérification du status en erreur du formulaire.
+     *
+     * @return bool
+     */
+    public function hasError(): bool;
+
+    /**
+     * Vérification d'activation de l'agencement des éléments.
+     *
+     * @return boolean
+     */
+    public function hasGrid();
+
+    /**
+     * Récupération du numéro d'indice du formulaire.
+     *
+     * @return int|null
+     */
+    public function index();
+
+    /**
+     * Vérification d'activation automatisée.
+     *
+     * @return boolean
+     */
+    public function isAuto(): bool;
+
+    /**
+     * Vérification de préparation active.
+     *
+     * @return boolean
+     */
+    public function isPrepared(): bool;
+
+    /**
+     * Récupération d'intitulé|Définition d'intitulés|Retourne l'instance du gestionnaire d'intitulés.
+     *
+     * @param string|array|null $key Clé d'indexe de l'intitulé.
+     * @param string $default Valeur de retour par défaut.
+     *
+     * @return LabelsBag|string
+     */
+    public function label($key = null, string $default = '');
+
+    /**
+     * Récupération du nom de qualification du formulaire.
+     *
+     * @return string
+     */
+    public function name(): string;
+
+    /**
+     * Evénement de déclenchement à l'initialisation du formulaire en tant que formulaire courant.
+     *
+     * @return void
+     */
+    public function onSetCurrent(): void;
+
+    /**
+     * Evénement de déclenchement à la réinitialisation du formulaire courant du formulaire.
+     *
+     * @return void
+     */
+    public function onResetCurrent(): void;
+
+    /**
+     * Initialisation (préparation) du champ.
+     *
+     * @return static
+     */
+    public function prepare(): FormFactory;
+
+    /**
+     * Affichage.
+     *
+     * @return string
+     */
+    public function render(): string;
+
+    /**
+     * Définition de l'instance.
+     *
+     * @param string $name Nom de qualification du formulaire.
+     * @param FormManager $manager Instance du gestionnaire de formulaires.
+     *
+     * @return static
+     */
+    public function setInstance(string $name, FormManager $manager): FormFactory;
+
+    /**
+     * Définition de l'indicateur de statut de formulaire en succès.
+     *
+     * @param boolean $status
+     *
+     * @return static
+     */
+    public function setOnSuccess(bool $status = true): FormFactory;
+
+    /**
+     * Récupération du nom de qualification du formulaire dans les attributs de balises HTML.
+     *
+     * @return string
+     */
+    public function tagName(): string;
+}
---
+++ tags/2.0.275/src/Field/FieldDriver.php
@@ -0,0 +1,327 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Field;
+
+use Closure;
+use tiFy\Contracts\Field\{Field as Manager, FieldDriver as FieldDriverContract};
+use tiFy\Contracts\View\Engine as ViewEngine;
+use tiFy\Support\{HtmlAttrs, ParamsBag, Str};
+
+abstract class FieldDriver extends ParamsBag implements FieldDriverContract
+{
+    /**
+     * Liste des attributs par défaut.
+     * @var array
+     */
+    protected static $defaults = [];
+
+    /**
+     * Indicateur d'initialisation.
+     * @var string
+     */
+    private $booted = false;
+
+    /**
+     * Alias de qualification dans le gestionnaire.
+     * @var string
+     */
+    private $alias = false;
+
+    /**
+     * Identifiant de qualification.
+     * {@internal par défaut concaténation de l'alias et de l'indice.}
+     * @var string
+     */
+    protected $id = '';
+
+    /**
+     * Indice de l'instance dans le gestionnaire.
+     * @var int
+     */
+    protected $index = 0;
+
+    /**
+     * Instance du gestionnaire de portions d'affichage.
+     * @var Manager
+     */
+    protected $manager;
+
+    /**
+     * Instance du moteur de gabarits d'affichage.
+     * @return ViewEngine
+     */
+    protected $viewer;
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return (string)$this->render();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function after(): void
+    {
+        echo ($after = $this->get('after', '')) instanceof Closure ? call_user_func($after) : $after;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function attrs(): void
+    {
+        echo HtmlAttrs::createFromAttrs($this->get('attrs', []));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function before(): void
+    {
+        echo ($before = $this->get('before', '')) instanceof Closure ? call_user_func($before) : $before;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function boot(): void {}
+
+    /**
+     * @inheritDoc
+     */
+    public function content(): void
+    {
+        echo ($content = $this->get('content', '')) instanceof Closure ? call_user_func($content) : $content;
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return array
+     */
+    public function defaults(): array
+    {
+        return array_merge(static::$defaults, []);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAlias(): string
+    {
+        return $this->alias;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getIndex(): int
+    {
+        return $this->index;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getName(): string
+    {
+        return $this->get('attrs.name', '') ? : $this->get('name');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getValue()
+    {
+        return $this->get('value', null);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function manager(): ?Manager
+    {
+        return $this->manager;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return $this
+     */
+    public function parse(): FieldDriverContract
+    {
+        $this->attributes = array_merge(
+            $this->defaults(), config("field.driver.{$this->alias}", []), $this->attributes
+        );
+
+        $this->parseDefaults();
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseAttrClass(): FieldDriverContract
+    {
+        $base = 'Field' . ucfirst(preg_replace('/\./', '-', Str::camel($this->getAlias())));
+
+        $default_class = "{$base} {$base}--" . $this->getIndex();
+        if (!$this->has('attrs.class')) {
+            $this->set('attrs.class', $default_class);
+        } else {
+            $this->set('attrs.class', sprintf($this->get('attrs.class', ''), $default_class));
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return $this
+     */
+    public function parseAttrId(): FieldDriverContract
+    {
+        if (!$this->get('attrs.id')) {
+            $this->forget('attrs.id');
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseAttrName(): FieldDriverContract
+    {
+        if ($name = $this->get('name')) {
+            $this->set('attrs.name', $name);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseAttrValue(): FieldDriverContract
+    {
+        if ($value = $this->get('value')) {
+            $this->set('attrs.value', $value);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseDefaults(): FieldDriverContract
+    {
+        return $this->parseAttrId()->parseAttrClass()->parseAttrName()->parseAttrValue()->parseViewer();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseViewer(): FieldDriverContract
+    {
+        foreach($this->get('viewer', []) as $key => $value) {
+            $this->viewer()->params([$key => $value]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function prepare(string $alias, Manager $manager): FieldDriverContract
+    {
+        if (!$this->booted) {
+            $this->alias = $alias;
+            $this->manager = $manager;
+
+            $this->boot();
+
+            $this->booted = true;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render(): string
+    {
+        return (string)$this->viewer('index', $this->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function setDefaults(array $defaults = []): void
+    {
+        static::$defaults = $defaults;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setId(string $id): FieldDriverContract
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setIndex(int $index): FieldDriverContract
+    {
+        $this->index = $index;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setViewer(ViewEngine $viewer): FieldDriverContract
+    {
+        $this->viewer = $viewer;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function viewer(?string $view = null, array $data = [])
+    {
+        if (is_null($this->viewer)) {
+            $this->viewer = app()->get('field.viewer', [$this]);
+        }
+
+        if (func_num_args() === 0) {
+            return $this->viewer;
+        }
+
+        return $this->viewer->render("{$view}", $data);
+    }
+}
