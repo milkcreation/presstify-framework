12d5f5cafe48c36c8884c74c114dd140a5a38afe
Jordy Manner <jordy.manner@milkcreation.fr>
2020-03-22 18:37:56 +0000

    2.0.278 -- Correctifs assets/theme + Ajustements FormFactory (verify) + BaseController debug + QueryPost (getTerms > array)

---
+++ src/Wordpress/Database/Model/Attachment.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Model;
+
+use Corcel\Model\Attachment as CorcelAttachment;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+class Attachment extends CorcelAttachment
+{
+
+}
