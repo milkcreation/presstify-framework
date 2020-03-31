3c14449ff6e991b70fb0dcc725e51c999c90f22a
Jordy Manner <jordy.manner@milkcreation.fr>
2020-03-31 06:27:17 +0000

    2.0.281 -- Correctif Mailer + Ajustement Model + Metabox Profile utilisateur

---
+++ src/Auth/Resources/views/signin/form/signin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Formulaire d'authentification | Gabarit.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Auth\Signin\SigninView $this
+ */
+?>
+<?php $this->insert('form/header', $this->all()); ?>
+
+<?php $this->insert('form/body', $this->all()); ?>
+
+<?php $this->insert('form/footer', $this->all());
---
+++ src/Support/Proxy/Column.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Support\Proxy;
+
+use tiFy\Contracts\Column\{Column as ColumnContract, ColumnItem as ColumnDriverContract};
+
+/**
+ * @method static add(string $screen, string $name, array|ColumnDriverContract $attrs)
+ * @method static stack(string $screen, array[]|ColumnDriverContract[] $attrs)
+ */
+class Column extends AbstractProxy
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @return ColumnContract
+     */
+    public static function getInstance()
+    {
+        return parent::getInstance();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getInstanceIdentifier()
+    {
+        return 'column';
+    }
+}
