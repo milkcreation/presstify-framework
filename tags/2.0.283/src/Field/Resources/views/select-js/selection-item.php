04a9094188cd4307d83d810d541f31f9492fb1bf
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-04-06 08:57:35 +0000

    2.0.283 -- Validation >> basée sur Respect\Validation 2.0

---
+++ tags/2.0.283/src/Field/Resources/views/select-js/selection-item.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Affichage d'un élément dans la liste des éléments sélectionnés.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Field\FieldView $this
+ * @var tiFy\Contracts\Field\SelectChoice $item
+ */
+echo $item->getContent();
