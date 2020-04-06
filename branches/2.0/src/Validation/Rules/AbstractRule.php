ef31457f527677fce9577719ff0fc236a10e1e41
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-04-06 08:55:35 +0000

    2.0 -- Validation >> basée sur Respect\Validation 2.0

--- branches/2.0/src/Validation/Rules/AbstractRule.php
+++ branches/2.0/src/Validation/Rules/AbstractRule.php
@@ -2,42 +2,16 @@
 
 namespace tiFy\Validation\Rules;
 
-use Psr\Container\ContainerInterface as Container;
 use Respect\Validation\Rules\AbstractRule as BaseAbstractRule;
 use tiFy\Contracts\Validation\Rule;
-use tiFy\Contracts\Validation\Validator;
 
 abstract class AbstractRule extends BaseAbstractRule implements Rule
 {
-    /**
-     * Instance du gestionnaire de validation.
-     * @var Validator
-     */
-    protected $validator;
-
-    /**
-     * @inheritDoc
-     */
-    public function getContainer(): ?Container
-    {
-        return $this->validator ? $this->validator->getContainer() : null;
-    }
-
     /**
      * @inheritDoc
      */
     public function setArgs(...$args): Rule
     {
-        return $this;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function setValidator(Validator $validator): Rule
-    {
-        $this->validator = $validator;
-
         return $this;
     }
 }
