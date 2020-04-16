375b563f3a302e03249e62f790001bc6e5f13bc9
Jordy Manner <jordy.manner@milkcreation.fr>
2020-04-16 07:11:50 +0000

    2.0 -- QueryPost deprecated commentaire OK + getAuthor

--- src/tiFy.php
+++ src/tiFy.php
@@ -10,7 +10,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.286
+ * @version 2.0.287
  * @copyright Milkcreation
  */
 final class tiFy extends Container
