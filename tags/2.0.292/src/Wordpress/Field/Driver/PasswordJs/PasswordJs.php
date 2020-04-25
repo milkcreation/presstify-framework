63a32e7b4e516e59bbc846a5ab3e7a1e7eb8faba
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-04-25 12:25:43 +0000

    2.0.292 -- Correctif Wp\Routing\Strategy + css findpots

---
+++ tags/2.0.292/src/Wordpress/Field/Driver/PasswordJs/PasswordJs.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Field\Driver\PasswordJs;
+
+use tiFy\Field\Driver\PasswordJs\PasswordJs as BasePasswordJs;
+use tiFy\Wordpress\Contracts\Field\FieldDriver as FieldDriverContract;
+
+class PasswordJs extends BasePasswordJs implements FieldDriverContract
+{
+
+}
