b6f8985b471f3b2a9cda5518cc58e07e8f7990b9
Jordy Manner <jordy.manner@milkcreation.fr>
2020-04-28 06:19:58 +0000

    2.0.293 -- Formulaire >> theme par défaut phase #1 + Form attr container + TextRemaining correctif + QueryTerm && QueryPost @property

---
+++ src/Mail/MailView.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Mail;
+
+use Exception;
+use BadMethodCallException;
+use tiFy\Contracts\Mail\{MailerDriver, MailView as MailViewContract};
+use tiFy\View\Factory\PlatesFactory;
+
+class MailView extends PlatesFactory implements MailViewContract
+{
+    /**
+     * Liste des méthodes héritées.
+     * @var array
+     */
+    protected $mixins = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, $this->mixins)) {
+            try {
+                return call_user_func_array([$this->engine->params('mailer'), $method], $parameters);
+            } catch (Exception $e) {
+                throw new BadMethodCallException(
+                    sprintf(
+                        __('La méthode [%s] du champ n\'est pas disponible.', 'tify'),
+                        $method
+                    )
+                );
+            }
+        } else {
+            return parent::__call($method, $parameters);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function driver(): MailerDriver
+    {
+        return $this->engine->params('mailer')->getDriver();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function linearizeContacts(array $contacts): array
+    {
+        array_walk($contacts, function (&$item) {
+            $item = isset($item[1]) ? "{$item[1]} <{$item[0]}>" : "{$item[0]}";
+        });
+
+        return $contacts;
+    }
+}
