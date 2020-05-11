b6e1f7bb3f8186a9f6b6be58a4b15162a2c63e23
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-05-11 11:27:28 +0000

    2.0.299 -- déps ramsey/uuid && Route >> Déclaration Controller + Middleware + Url paramètres qualifiés et complémentaires + QueryPost ArchiveUrl

---
+++ tags/2.0.299/src/Wordpress/Database/Model/Page.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Model;
+
+use Corcel\Model\Page as CorcelPage;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+class Page extends CorcelPage
+{
+
+}
