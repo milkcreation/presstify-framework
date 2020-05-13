583d6bc620b17de843ac02ffa311452da5ed529d
Jordy Manner <jordy.manner@milkcreation.fr>
2020-05-13 17:53:37 +0000

    2.0.300 -- Passe Formulaire >> version auto && method GET + QueryPost && QueryTerm >> builtIn + Correctif PageHook

---
+++ src/Wordpress/Database/Model/MenuItem.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Model;
+
+use Corcel\Model\MenuItem as CorcelMenuItem;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+class MenuItem extends CorcelMenuItem
+{
+
+}
