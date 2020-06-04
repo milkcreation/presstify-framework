da85c004d7d621cf80e5b2505e34a4a476ea0678
Jordy Manner <jordy.manner@milkcreation.fr>
2020-06-04 10:12:00 +0000

    2.0.307 -- Dépôt oublie Ajout

---
+++ src/Field/FieldView.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Field;
+
+use Exception;
+use BadMethodCallException;
+use tiFy\Contracts\Field\FieldView as FieldViewContract;
+use tiFy\View\Factory\PlatesFactory;
+
+/**
+ * @method string after()
+ * @method string attrs()
+ * @method string before()
+ * @method string content()
+ * @method string getAlias()
+ * @method string getId()
+ * @method string getIndex()
+ * @method string getName()
+ * @method string getValue()
+ */
+class FieldView extends PlatesFactory implements FieldViewContract
+{
+    /**
+     * Liste des méthodes héritées.
+     * @var array
+     */
+    protected $mixins = [
+        'after',
+        'attrs',
+        'before',
+        'content',
+        'getAlias',
+        'getId',
+        'getIndex',
+        'getName',
+        'getValue'
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, $this->mixins)) {
+            try {
+                return call_user_func_array([$this->engine->params('field'), $method], $parameters);
+            } catch (Exception $e) {
+                throw new BadMethodCallException(
+                    sprintf(
+                        __('La méthode [%s] du champ n\'est pas disponible.', 'tify'),
+                        $method
+                    )
+                );
+            }
+        } else {
+            return parent::__call($method, $parameters);
+        }
+    }
+}
---
+++ src/Wordpress/Option/Resources/views/standard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var tiFy\Wordpress\Option\OptionPageView $this
+ */
+?>
+<div>
+    <?php settings_fields($this->get('name')); ?>
+    <?php do_settings_sections($this->get('name')); ?>
+</div>
+
+<?php submit_button(); ?>
