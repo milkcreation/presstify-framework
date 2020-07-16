b70e5b4ba3f8366474268e24f646bbd7c0d064a8
Jordy Manner <jordy.manner@milkcreation.fr>
2020-07-16 16:16:19 +0000

    2.0.318 -- XStatic >> ReStatic

---
+++ src/Wordpress/Database/Model/Menu.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Database\Model;
+
+use Corcel\Model\Menu as CorcelMenu;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+class Menu extends CorcelMenu
+{
+
+}
