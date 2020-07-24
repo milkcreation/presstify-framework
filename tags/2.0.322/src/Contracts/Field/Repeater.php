ff09b52f03ecee32144b284c519ef53294a9ae02
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-07-24 09:37:38 +0000

    2.0.322 -- Breadcrumb >> Modification de la gestion du main

---
+++ tags/2.0.322/src/Contracts/Field/Repeater.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Contracts\Field;
+
+interface Repeater extends FieldDriver
+{
+    /**
+     * Récupération de l'url de traitement de la requête XHR.
+     *
+     * @param array ...$params Liste des paramètres optionnels de formatage de l'url.
+     *
+     * @return string
+     */
+    public function getUrl(...$params): string;
+
+    /**
+     * Définition de l'url de traitement de la requête XHR.
+     *
+     * @param string|null $url
+     *
+     * @return static
+     */
+    public function setUrl(?string $url =  null): Repeater;
+
+    /**
+     * Contrôleur de traitement de la requête XHR.
+     *
+     * @param array ...$args Liste dynamique de variables passés en argument dans l'url de requête
+     *
+     * @return array
+     */
+    public function xhrResponse(...$args): array;
+}
---
+++ tags/2.0.322/src/Wordpress/Field/Driver/Repeater/Repeater.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Wordpress\Field\Driver\Repeater;
+
+use tiFy\Field\Driver\Repeater\Repeater as BaseRepeater;
+use tiFy\Wordpress\Contracts\Field\FieldDriver as FieldDriverContract;
+
+class Repeater extends BaseRepeater implements FieldDriverContract
+{
+
+}
