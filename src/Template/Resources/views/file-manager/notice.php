8817990855a9b0dea6bbf2f7cc95b808e01e2388
Jordy Manner <jordy.manner@milkcreation.fr>
2020-07-31 16:42:23 +0000

    2.0.224 - Correctif suggest.js - Session : Correctif Bug démarrage depuis FlashNotice + Middleware de lancement de la session - Wordpress\Routing\BaseController : Gestion de ImagePlaceHolder

---
+++ src/Template/Resources/views/file-manager/notice.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Gestionnaire de fichiers > Fichier.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\FileManager\View $this
+ */
+?>
+<div class="FileManager-notice" data-control="file-manager.notice"></div>
