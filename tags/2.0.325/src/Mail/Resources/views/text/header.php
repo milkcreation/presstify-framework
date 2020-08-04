029fa889e3bddef69b8e3e236576b151260ae93d
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-08-04 09:22:05 +0000

    Erreur de dépôt

--- tags/2.0.325/src/Mail/Resources/views/text/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-/**
- * @var tiFy\Contracts\Mail\MailView $this
- */
--- tags/2.0.325/src/Template/Resources/views/list-table/extras.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Interface de navigation complémentaire.
- * ---------------------------------------------------------------------------------------------------------------------
- * @var tiFy\Template\Templates\ListTable\View $this
- * @var string $which top|bottom.
- */
-echo $this->extras($which);
