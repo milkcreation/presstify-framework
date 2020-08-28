82e5e8ac9ade0aa78aba022f5b224f42d1b82593
Jordy Manner <jordy.manner@milkcreation.fr>
2020-08-28 14:36:46 +0000

    2.0.332 -- - assets/form/scss : Gestion des types de champs email et tel - View : modification de gestion du répertoire par défaut - Wordpress\View : Définition du répertoire par défaut comme étant celui du thème - Field\Checkox + Field\Radio: Modification de la valeur par défaut de checked > bool - BaseController : méthode share depuis Wordpress\BaseController

---
+++ src/Console/Command.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Console;
+
+use Exception;
+use Symfony\Component\Console\{
+    Command\Command as BaseCommand,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+use tiFy\Contracts\Console\Command as CommandContract;
+
+/**
+ * USAGE :
+ * Liste des commandes disponibles
+ * -------------------------------
+ * php console list
+ *
+ * TIPS :
+ * Arrêt complet des commandes CLI lancées
+ * ---------------------------------------
+ * pkill -9 php
+ */
+class Command extends BaseCommand implements CommandContract
+{
+    /**
+     * @inheritDoc
+     */
+    protected function configure() { }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws Exception
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        return 0;
+    }
+}
---
+++ src/Template/Resources/views/list-table/table-after.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Préaffichage de la table.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Template\Templates\ListTable\View $this
+ */
+echo $this->param('table.after', '');
