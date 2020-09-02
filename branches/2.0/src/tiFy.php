0c12d85ca54dcd79ef31ae25734d9a80067a55b1
5upp0r7 <5upp0r7@59481347-5d8a-4110-b049-6281aa622e7a>
2020-09-02 06:21:55 +0000

    2.0.333 -- - assets/partial/accordion/js : Suppression de la gestion via data-control à la faveur des classes - assets/partial/pdfviewer/js : Modification de la surchage jQuery tiFyModal - assets/partial/pdfviewer/css : Fixe la taille de la modal pdf à 640px - Partial/Driver/Modal : backdrop close à false par défaut - Partial/Driver/Sidebar : Correctif gestion des styles

--- branches/2.0/src/tiFy.php
+++ branches/2.0/src/tiFy.php
@@ -12,7 +12,7 @@ use tiFy\Kernel\KernelServiceProvider;
  * @desc PresstiFy -- Framework Milkcreation.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package tiFy
- * @version 2.0.332
+ * @version 2.0.333
  * @copyright Milkcreation
  */
 final class tiFy extends Container
