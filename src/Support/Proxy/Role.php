d317238765c80943c458e1e64f32fd58c360fcac
Jordy Manner <jordy.manner@milkcreation.fr>
2020-09-02 06:22:14 +0000

    2.0.333 -- - assets/partial/accordion/js : Suppression de la gestion via data-control à la faveur des classes - assets/partial/pdfviewer/js : Modification de la surchage jQuery tiFyModal - assets/partial/pdfviewer/css : Fixe la taille de la modal pdf à 640px - Partial/Driver/Modal : backdrop close à false par défaut - Partial/Driver/Sidebar : Correctif gestion des styles

---
+++ src/Support/Proxy/Role.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Support\Proxy;
+
+use tiFy\Contracts\User\{RoleManager, RoleFactory};
+
+/**
+ * @method static RoleFactory|null get(string $name)
+ * @method static RoleManager|null register(string $name, array|RoleFactory $attrs = [])
+ * @method static RoleManager|null set(string|array $role_name, array|RoleFactory|null $attrs = null)
+ */
+class Role extends AbstractProxy
+{
+    /**
+     * @inheritDoc
+     */
+    public static function getInstanceIdentifier(): string
+    {
+        return 'user.role';
+    }
+}
