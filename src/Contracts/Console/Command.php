7a2984cfad9e44f9ac3c267d95e775bf8d03b105
Jordy Manner <jordy.manner@milkcreation.fr>
2020-09-18 08:19:26 +0000

    v2.0.341 >> branch

--- src/Contracts/Console/Command.php
+++ src/Contracts/Console/Command.php
@@ -3,6 +3,9 @@
 namespace tiFy\Contracts\Console;
 
 use Symfony\Component\Console\Command\Command as BaseCommand;
+use Symfony\Component\Console\Output\OutputInterface;
+use tiFy\Contracts\Log\Logger as LoggerContract;
+use tiFy\Support\MessagesBag;
 
 /**
  * USAGE :
@@ -21,5 +24,45 @@ use Symfony\Component\Console\Command\Command as BaseCommand;
  */
 interface Command
 {
+    /**
+     * Traitement des messages de notification.
+     *
+     * @param OutputInterface $output
+     * @param bool $forget Suppression des messages
+     *
+     * @return void
+     */
+    public function handleNotices(OutputInterface $output, bool $forget = true): void;
 
+    /**
+     * Journalisation.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array $context
+     *
+     * @return string|LoggerContract|null
+     */
+    public function log($level = null, string $message = '', array $context = []);
+
+    /**
+     * Ajout d'un message ou récupération de l'instance du gestionnaire de message.
+     *
+     * @param string|int|null $level
+     * @param string $message
+     * @param array|null $data
+     * @param string $code
+     *
+     * @return MessagesBag|string|null
+     */
+    public function message($level = null, string $message = null, ?array $data = [], ?string $code = null);
+
+    /**
+     * Définition de la journalisation.
+     *
+     * @param LoggerContract|bool $offset
+     *
+     * @return static
+     */
+    public function setLogger($logger = true): Command;
 }
