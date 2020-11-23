cf9acb98a28b30260448eb34776c38eb63550240
Jordy Manner <jordy.manner@milkcreation.fr>
2020-11-23 11:39:01 +0000

    framework v2.0.364

---
+++ README.md
@@ -0,0 +1,10 @@
+# Contact Infos PresstiFy Plugin
+
+[![Latest Version](https://img.shields.io/badge/release-2.0.364-blue?style=for-the-badge)](https://svn.tigreblanc.fr/presstify/framework/tags/2.0.364)
+[![MIT Licensed](https://img.shields.io/badge/license-MIT-green?style=for-the-badge)](LICENSE.md)
+
+## Installation
+
+```bash
+composer require presstify/framework
+```
---
+++ src/Mail/Resources/views/mailable/html/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @var tiFy\Mail\MailableView $this
+ */
+?>
+<?php if ($logo = $this->get('infos.logo')) : ?>
+    <tr class="rowHeaderContent">
+        <td>
+            <?php echo is_array($logo) ? partial('tag', [
+                'attrs' => [
+                    'class'  => 'BodyHeader-logo',
+                    'src'    => $logo['src'] ?? '',
+                    'width'  => $logo['width'] ?? 200,
+                    'height' => $logo['height'] ?? 40,
+                    'alt'    => $logo['alt'] ?? __('Logo', 'tify'),
+                    'border' => 0,
+                ],
+                'tag'   => 'img',
+            ]) : $logo; ?>
+        </td>
+    </tr>
+<?php endif;
