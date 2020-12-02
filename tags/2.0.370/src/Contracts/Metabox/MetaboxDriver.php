7177ce4deb022515811e67a47af13f287495ab11
mast3rm1lk <mast3rm1lk@59481347-5d8a-4110-b049-6281aa622e7a>
2020-12-02 14:14:42 +0000

    framework v2.0.370

---
+++ tags/2.0.370/src/Contracts/Metabox/MetaboxDriver.php
@@ -0,0 +1,210 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Contracts\Metabox;
+
+use Closure;
+use tiFy\Contracts\Support\ParamsBag;
+use tiFy\Contracts\View\View as ViewEngine;
+
+interface MetaboxDriver extends ParamsBag
+{
+    /**
+     * Résolution de sortie de la classe sous forme d'une chîane de caractères.
+     *
+     * @return string
+     */
+    public function __toString(): string;
+
+    /**
+     * Chargement.
+     *
+     * @return static
+     */
+    public function boot(): MetaboxDriver;
+
+    /**
+     * Initialisation.
+     *
+     * @return static
+     */
+    public function build(): MetaboxDriver;
+
+    /**
+     * Liste des paramètres par défaut.
+     *
+     * @return array
+     */
+    public function defaultParams(): array;
+
+    /**
+     * Valeur(s) par défaut.
+     *
+     * @return mixed
+     */
+    public function defaultValue();
+
+    /**
+     * Récupération de l'identifiant de qualification dans le gestionnaire.
+     *
+     * @return string
+     */
+    public function getAlias(): string;
+
+    /**
+     * Récupération de l'instance du contexte d'affichage associé.
+     *
+     * @return MetaboxContext|null
+     */
+    public function getContext(): ?MetaboxContext;
+
+    /**
+     * Récupération de l'instance de l'écran d'affichage associé.
+     *
+     * @return MetaboxScreen|null
+     */
+    public function getScreen(): ?MetaboxScreen;
+
+    /**
+     * Récupération de l'identifiant de qualification unique.
+     *
+     * @return string
+     */
+    public function getUuid(): string;
+
+    /**
+     * Traitement
+     *
+     * @param array $args Liste des arguments de traitement
+     *
+     * @return static
+     */
+    public function handle(array $args = []): MetaboxDriver;
+
+    /**
+     * Récupération de l'instance du gestionnaire.
+     *
+     * @return Metabox|null
+     */
+    public function metabox(): ?Metabox;
+
+    /**
+     * Récupération du nom de qualification dans la requête d'enregistrement des données.
+     *
+     * @return string
+     */
+    public function name(): string;
+
+    /**
+     * Récupération de l'instance des paramètres|récupération d'un paramètre|Définition de paramètres.
+     *
+     * @param string|array|null $key Clé d'indice du paramètres. Syntaxe à point permise.
+     * @param mixed $default Valeur de retour par défaut.
+     *
+     * @return mixed
+     */
+    public function params($key = null, $default = null);
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return static
+     */
+    public function parse(): ?MetaboxDriver;
+
+    /**
+     * Récupération de l'affichage du contenu de la boîte de saisie.
+     *
+     * @return string
+     */
+    public function render(): string;
+
+    /**
+     * Définition de l'alias de qualification.
+     *
+     * @param string $alias
+     *
+     * @return static
+     */
+    public function setAlias(string $alias): MetaboxDriver;
+
+    /**
+     * Définition de l'instance du contexte d'affichage.
+     *
+     * @param string $alias
+     *
+     * @return static
+     */
+    public function setContext(string $alias): MetaboxDriver;
+
+    /**
+     * Définition de valeur(s) par défaut.
+     *
+     * @param mixed $value
+     *
+     * @return static
+     */
+    public function setDefaultValue($value = null): MetaboxDriver;
+
+    /**
+     * Définition d'une fonction de traitement.
+     *
+     * @param Closure $func
+     *
+     * @return static
+     */
+    public function setHandler(Closure $func): MetaboxDriver;
+
+    /**
+     * Définition de l'instance du gestionnaire.
+     *
+     * @param Metabox $metabox
+     *
+     * @return static
+     */
+    public function setMetabox(Metabox $metabox): MetaboxDriver;
+
+    /**
+     * Définition de l'instance de l'écran d'affichage.
+     *
+     * @param string $alias
+     *
+     * @return static
+     */
+    public function setScreen(string $alias): MetaboxDriver;
+
+    /**
+     * Récupération de l'intitulé de qualification.
+     *
+     * @return string
+     */
+    public function title(): string;
+
+    /**
+     * Récupération de la valeur courante.
+     *
+     * @param string|null $key Indice de qualification de la valeur. Syntaxe à point permise.
+     * @param mixed $default Valeur de retour par défaut.
+     *
+     * @return mixed
+     */
+    public function value(?string $key = null, $default = null);
+
+    /**
+     * Récupération d'un instance du controleur de liste des gabarits d'affichage ou d'un gabarit d'affichage.
+     * {@internal Si aucun argument n'est passé à la méthode, retourne l'instance du controleur de liste.}
+     * {@internal Sinon récupére l'instance du gabarit d'affichage et passe les variables en argument.}
+     *
+     * @param string|null view Nom de qualification du gabarit.
+     * @param array $data Liste des variables passées en argument.
+     *
+     * @return ViewEngine|string
+     */
+    public function view(?string $view = null, array $data = []);
+
+    /**
+     * Chemin absolu du répertoire des gabarits d'affichage.
+     *
+     * @return string
+     */
+    public function viewDirectory(): string;
+}
---
+++ tags/2.0.370/src/Form/FieldController.php
@@ -0,0 +1,110 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Form;
+
+use tiFy\Contracts\Form\FactoryField;
+use tiFy\Contracts\Form\FieldController as FieldControllerContract;
+use tiFy\Form\Factory\ResolverTrait;
+
+class FieldController implements FieldControllerContract
+{
+    use ResolverTrait;
+
+    /**
+     * Liste des attributs de support des types de champs natifs.
+     * @var array
+     */
+    protected $fieldSupports = [
+        'button'              => ['request', 'wrapper'],
+        'checkbox'            => ['checking', 'label', 'request', 'wrapper', 'session', 'tabindex', 'transport'],
+        'checkbox-collection' => ['choices', 'label', 'request', 'session', 'tabindexes', 'transport', 'wrapper'],
+        'datetime-js'         => ['label', 'request', 'session', 'tabindexes', 'transport', 'wrapper'],
+        'file'                => ['label', 'request', 'tabindex', 'wrapper'],
+        'hidden'              => ['request', 'session', 'transport'],
+        'label'               => ['wrapper'],
+        'password'            => ['label', 'request', 'tabindex', 'wrapper'],
+        'radio'               => ['label', 'request', 'session', 'tabindex', 'transport', 'wrapper'],
+        'radio-collection'    => ['choices', 'label', 'request', 'session', 'tabindexes', 'transport', 'wrapper'],
+        'repeater'            => ['label', 'request', 'session', 'tabindexes', 'transport', 'wrapper'],
+        'select'              => ['choices', 'label',  'request', 'session', 'tabindex', 'transport', 'wrapper'],
+        'select-js'           => ['choices', 'label', 'request', 'session', 'tabindex', 'transport', 'wrapper'],
+        'submit'              => ['request', 'tabindex', 'wrapper'],
+        'toggle-switch'       => ['request', 'tabindex', 'session', 'transport', 'wrapper'],
+    ];
+
+    /**
+     * Nom de qualification (type).
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * Liste des propriétés de support.
+     * @var array
+     */
+    protected $supports = ['label', 'request', 'session', 'tabindex', 'transport', 'wrapper'];
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param string $name Nom de qualification.
+     * @param FactoryField $field Instance du contrôleur de champ de formulaire associé.
+     *
+     * @void
+     */
+    public function __construct(string $name, FactoryField $field)
+    {
+        $this->name = $name;
+        $this->field = $field;
+        $this->form = $field->form();
+
+        $this->boot();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return (string)$this->render();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports()
+    {
+        if (isset($this->fieldSupports[$this->field()->getType()])) {
+            return $this->fieldSupports[$this->field()->getType()];
+        } else {
+            return $this->supports;
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render()
+    {
+        $args = array_merge($this->field()->getExtras(), [
+            'name'  => $this->field()->getName(),
+            'attrs' => $this->field()->get('attrs', [])
+        ]);
+
+        if($this->field()->supports('choices')) {
+            $args['choices'] = $this->field()->get('choices', []);
+        }
+
+        $args['value'] = $this->field()->getValue();
+
+        return field($this->field()->getType(), $args);
+    }
+}
