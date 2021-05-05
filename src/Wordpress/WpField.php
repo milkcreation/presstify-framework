2d48b6489508d672c73030356d82529c74154749
Jordy Manner <jordy.manner@milkcreation.fr>
2021-05-05 09:51:51 +0000

    presstify-framework >> 2.2.x-dev

--- src/Wordpress/WpField.php
+++ src/Wordpress/WpField.php
@@ -7,8 +7,6 @@ namespace tiFy\Wordpress;
 use Pollen\Support\Proxy\ContainerProxy;
 use Psr\Container\ContainerInterface as Container;
 use Pollen\Field\FieldManagerInterface;
-use Pollen\Field\Drivers\FileJsDriver as BaseFileJsDriver;
-use Pollen\Field\Drivers\SuggestDriver as BaseSuggestDriver;
 use tiFy\Wordpress\Field\Drivers\FileJsDriver;
 use tiFy\Wordpress\Field\Drivers\FindpostsDriver;
 use tiFy\Wordpress\Field\Drivers\MediaFileDriver;
@@ -24,9 +22,11 @@ class WpField
      * @var array
      */
     protected $drivers = [
+        'file-js'     => FileJsDriver::class,
         'findposts'   => FindpostsDriver::class,
         'media-file'  => MediaFileDriver::class,
         'media-image' => MediaImageDriver::class,
+        'suggest'     => SuggestDriver::class
     ];
 
     /**
@@ -43,58 +43,8 @@ class WpField
         $this->field = $field;
         $this->setContainer($container);
 
-        $this->registerDrivers();
-        $this->registerOverride();
-
         foreach ($this->drivers as $name => $alias) {
             $this->field->register($name, $alias);
         }
-    }
-
-    /**
-     * Déclaration des pilotes spécifiques à Wordpress.
-     *
-     * @return void
-     */
-    public function registerDrivers(): void
-    {
-        $this->containerAdd(
-            FindpostsDriver::class,
-            function () {
-                return new FindpostsDriver($this->containerGet(FieldManagerInterface::class));
-            }
-        );
-
-        $this->containerAdd(
-            MediaFileDriver::class,
-            new MediaFileDriver($this->containerGet(FieldManagerInterface::class))
-        );
-
-        $this->containerAdd(
-            MediaImageDriver::class,
-            new MediaImageDriver($this->containerGet(FieldManagerInterface::class))
-        );
-    }
-
-    /**
-     * Déclaration des surchages de pilote de champs.
-     *
-     * @return void
-     */
-    public function registerOverride(): void
-    {
-        $this->containerAdd(
-            BaseFileJsDriver::class,
-            function () {
-                return new FileJsDriver($this->containerGet(FieldManagerInterface::class));
-            }
-        );
-
-        $this->containerAdd(
-            BaseSuggestDriver::class,
-            function () {
-                return new SuggestDriver($this->containerGet(FieldManagerInterface::class));
-            }
-        );
     }
 }
